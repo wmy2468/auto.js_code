f7ecbb140ed9f73e712819f360873c64fad3b443
JJ-LAPTOP <xuiewy@163.com>
2020-04-26 23:25:05 +0800

    浦发签到更新

--- 浦发签到.js
+++ 浦发签到.js
@@ -6,21 +6,28 @@ run_app(pkg);
 var text = "我的";
 wait_text_load(pkg, text);
 text_click(text);
-// 等待加载 我的
-text = "注册 / 登录"
-wait_text_load(pkg, text);
-// 点击 签到
-id_click("iv_user_leader_title_3");
+var id = "iv_user_leader_title_3";
+// 等待 签到 加载 并点击
+wait_id_load(pkg, id);
+id_click(id);
 // 等待签到页面加载
 var clsname = "android.view.View";
-text = "今天"
+text = "今天";
 wait_text_load(pkg, text, clsname);
-click(288 + (792-288)/2, 1081 + (1177-1081)/2);     //点击立即签到
-text = "指纹验证"
+text = "今日已签到，明天再来吧！";
+// 如果找到今天已经签到 则退出
+if (item_is_load(pkg, text, clsname)) {
+    toastLog("今天已签到，退出");
+    exit();
+}
+else {
+    click(288 + (792 - 288) / 2, 1081 + (1177 - 1081) / 2); //点击立即签到
+}
+text = "指纹验证";
 wait_text_load(pkg, text);
-text = "我知道了"
-wait_text_load(pkg, text);
-text_click(text);
+text = "我知道了";
+wait_text_load(pkg, text, clsname);
+text_click(text, clsname);
 
 //run_app("com.spdbccc.app");
 //app_status("com.spdbccc.app.activity.MainActivity");
@@ -31,34 +38,67 @@ text_click(text);
 
 //等待元素
 function wait_text_load(pkg, text, className) {
-    if (className == undefined) {className = "android.widget.TextView"}
-    if (packageName(pkg).text(text).className(className).findOne() != null) {
-        toastLog(pkg + " 文本： " + text + " 已加载");
-        return true;
-    }
-    else 
-    {
-        toastLog(pkg + " 文本： " + text + " 未加载");
-        return false;
-    }    
-}
-
-//点击元素
-function text_click(text, className) {
     if (className == undefined) {
         className = "android.widget.TextView"
     }
-    toastLog("text: " + text);
-    var po = className(className).text(text).findOnce().parent().bounds();
+    if (packageName(pkg).text(text).className(className).findOne() != null) {
+        log(pkg + " 文本： " + text + " 已加载");
+        return true;
+    } else {
+        log(pkg + " 文本： " + text + " 未加载");
+        return false;
+    }
+}
+
+
+function wait_id_load(pkg, id, className) {
+    if (className == undefined) {
+        className = "android.widget.ImageView"
+    }
+    if (packageName(pkg).id(id).className(className).findOne() != null) {
+        log(pkg + " id： " + id + " 已加载");
+        return true;
+    } else {
+        log(pkg + " id： " + id + " 未加载");
+        return false;
+    }
+}
+
+
+function item_is_load(pkg, text, clsName) {
+    if (clsName == undefined) {
+        clsName = "android.widget.ImageView"
+    }
+    if (packageName(pkg).text(text).className(clsName).findOnce(1) != null) {
+        log(pkg + " text " + id + " 已加载");
+        return true;
+    } else {
+        log(pkg + " text " + id + " 未加载");
+        return false;
+    }
+}
+
+//点击元素
+function text_click(text, clsName) {
+    if (clsName == undefined) {
+        clsName = "android.widget.TextView";
+    }
+    log("点击元素text " + text);
+    var po = className(clsName).text(text).findOnce().parent().bounds();
     click(po.centerX(), po.centerY());
 }
 
 
 //点击元素
-function id_click(text) {
-    toastLog("id: " + text);
-    var po = className("android.widget.TextView").id(text).findOnce().parent().bounds();
-    click(po.centerX(), po.centerY());
+function id_click(text, clsName) {
+    log("id: " + text);
+    if (clsName == undefined) {
+        clsName = "android.widget.ImageView";
+    }
+    log("点击元素id: " + id);
+    if (className(clsName).id(text).findOnce() != null) {
+        className(clsName).id(text).findOnce().click();
+       }
 }
 
 
@@ -66,11 +106,10 @@ function id_click(text) {
 function run_app(pac_name) {
     while (true) {
         if (currentPackage() != pac_name) {
-            toastLog("当前未处于 " + pac_name + " 中，执行打开...");
+            log("当前未处于 " + pac_name + " 中，执行打开...");
             app.launch(pac_name);
-            sleep(4000);
-        }
-        else {
+            sleep(3000);
+        } else {
             sleep(1000);
             break;
         }
