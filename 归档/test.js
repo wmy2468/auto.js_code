aecb895ff26f584c20a18d82a28e72ff4efdf300
JJ Chen <mw03251214@163.com>
2020-09-03 23:26:20 +0800

    123

--- 归档/test.js
+++ 归档/test.js
@@ -4,8 +4,16 @@
 // 商品点击
 //log(text('逛商品').findOnce().parent().parent().child(3).child(1).click());
 
-log(center_click(className('ImageView').desc('浮层活动').findOnce().parent()));
-
+sClick(text('招钱进宝').findOne());
+function sClick(element) {
+    if (element != null) {
+        if (!element.click()) {
+            click(element.bounds().centerX(), element.bounds().centerY());
+            return 1;
+        }
+    }
+    return 0;
+}
 
 function center_click(element) {
     if (element != null) {
--- 归档/虚拟定位精灵.js
+++ 归档/虚拟定位精灵.js
@@ -1,6 +1,7 @@
 let name = '18107523694';
 let pwd = 'zzc1231';
 let pointText = '泉州';
+
 main();
 
 function passAd() {
@@ -47,4 +48,28 @@ function main() {
     setText(1, pwd);
     sClick(id('bt_login').findOne());
     sClick(text('招钱进宝').findOne());
+    // 等待钱宝加载
+    let myBtn = id('com.example.mposstandard:id/main_tab_my').text('我的').findOne();
+    sleep(4000);
+    sClick(myBtn);
+    text('我的资产').findOne();
+    sleep(800);
+    let clickLogin = text('点击登录').findOnce();
+    if (clickLogin == null) {
+        alert('当前已登录');
+        return 0;
+    } else {
+        sClick(clickLogin);
+        sleep(500);
+    }
+    let loginBtn = id('com.example.mposstandard:id/title_id').text('登 录').findOne();
+    let qbName = '15980998207';
+    let qbPwd = '003451jj';
+    setText(0, qbName);
+    setText(1, qbPwd);
+    sleep(800);
+    sClick(loginBtn);
+    text('发送账单').id('com.example.mposstandard:id/bill_submit_button').findOne();
+    sleep(500);
+    alert('当前已登录');
 }
