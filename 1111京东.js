eadd17df05e51acb4d06a439731c9dbf56f33459
JJ Chen <mw03251214@163.com>
2020-10-28 19:10:16 +0800

    1

--- 1111京东.js
+++ 1111京东.js
@@ -46,6 +46,8 @@ taskList.forEach(task => {
 				if (indexText.indexOf('秒') != -1) { nextStep = '等待8秒' }
 				if (indexText.indexOf('浏览可得') != -1) { nextStep = '浏览返回' }
 				if (indexText.indexOf('浏览5个') != -1) { nextStep = '浏览商品' }
+				if (indexText.indexOf('加购5个') != -1) { nextStep = '加购物车' }
+				if (indexText.indexOf('成功入会') != -1) { nextStep = '加入会员' }
 				func.sClick(unComplete[index]);
 				sleep(1000);
 				after_click(nextStep);
@@ -53,6 +55,7 @@ taskList.forEach(task => {
 		}
 		sleep(1000);
 	}
+	alert('已完成');
 });
 
 function after_click(textStr) {
@@ -67,15 +70,20 @@ function after_click(textStr) {
 				sleep(1000);
 			}
 			sleep(400);
-			back_way();
 			break;
 		case '浏览返回':
 			sleep(5000);
-			back_way()
 			break;
 		case '浏览商品':
 			view_list();
 			break;
+		case '加购物车':
+			add_cart();
+			break;
+		case '加入会员':
+			member_card();
+			break;
+
 		default:
 			break;
 	}
@@ -84,20 +92,52 @@ function after_click(textStr) {
 	sleep(2000);
 }
 
+// 浏览5个商品
 function view_list() {
 	i = 0;
 	while (text('已完成').findOnce() == null) {
 		textContains('¥').findOne();
 		//点击商品加购物车按钮
-		(textContains('¥').find()[i]).parent().parent();
+		func.cClick((textContains('¥').find()[i]).parent().parent());
 		textContains('购物车').findOne();
-		sleep(1500);
+		sleep(800);
 		back_way();
-		sleep(2500);
+		sleep(2000);
 		i = i + 1;
 	}
 }
-
+// 加入会员
+function member_card() {
+	let count = 0;
+	//toastLog('会员卡');
+	sleep(4000);
+	if (textContains('邀请好友助力').findOnce() != null) {
+		exit();
+	}
+	while (textContains('已集齐所有会员卡').findOnce() == null) {
+		func.sClick(textContains('确认授权并加入').findOnce())
+		count = count + 1;
+		sleep(4000);
+	}
+}
+//加购5个商品
+function add_cart() {
+	i = 0;
+	textContains('当前页任意加购5个商品').findOne();
+	while (text('已完成').findOnce() == null) {
+		if (i >= 4) {
+			swipe(500, 800, 500, 300, 300);
+			sleep(4000);
+		}
+		textContains('¥').findOne();
+		//点击商品加购物车按钮
+		func.sClick((textContains('¥').find()[i]).parent().parent().child(4));
+		//textContains('购物车').findOne();
+		//back_way();
+		sleep(2000);
+		i = i + 1;
+	}
+}
 
 function back_way() {
 	sleep(800);
--- test.js
+++ test.js
@@ -1,1 +1,2 @@
-toastLog(className('TextView').text('立即签到').findOnce().click());
+//toastLog(textContains('¥').findOne().parent().parent().click());
+swipe(500, 800, 500, 300, 300);
