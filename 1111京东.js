c37357b01d8e0a14817dad5d1762e98a24c2f010
JJ-Company <JJ@linde.com>
2020-11-04 08:46:39 +0800

    a

--- 1111京东.js
+++ 1111京东.js
@@ -151,7 +151,7 @@ function 营业版图_去完成() {
 		toastLog('去完成剩余 = ' + unComplete.length);
 		if (unComplete.nonEmpty()) {
 			let goUnComplete = unComplete[index];
-			toastLog(unComplete[goUnComplete.indexInParent()-1].text());
+			toastLog(unComplete.parent().child([goUnComplete.indexInParent()-1]).text());
 			func.sClick(goUnComplete);
 			log('点击去完成');
 			while (text('签到得最高500金币').findOnce() != null) {
