930ffd7ddb2d65da3bf8ddeb062b6b45d4177e01
JJ-LAPTOP <xuiewy@163.com>
2020-12-13 09:04:48 +0800

    a

--- update all.js
+++ update all.js
@@ -10,20 +10,44 @@ var jsFiles = files.listDir(dir, function (name) {
 });
 
 var fileName, filePath, fileUrl;
-for (i = 0; i <= jsFiles.length; i++) {
-    fileName = jsFiles[i];              // 文件名
+var selectedArr = ["更新所有文件"];
+var selectIndex = dialogs.select('选择功能', selectedArr);
+if (selectIndex == -1) {
+    fileName = rawInput("请输入要下载的文件名,例:XXX，不需要输入.JS");
+    if (fileName == null || fileName == '') {
+        toastLog("未输入文件名，退出");
+        exit();
+    }
+    if (fileName.indexOf('.js') == -1) {
+        fileName = fileName + '.js';
+    }
     filePath = dir + '/' + fileName;    // 文件路径
     fileUrl = originUrl + fileName;     // 网络文件路径
     var req = http.get(fileUrl);
     if (req.statusCode != '200') {
-        toastLog('网络读取错误，可能文件不存在，继续下一个' + req.statusMessage)
-        //exit();
+        toastLog('网络读取错误，可能文件不存在' + req.statusMessage)
+        sleep(800);
+        alert('更新失败 退出');
     } else {
-        toastLog('更新完成 写入文件' + req.statusMessage)
         // 写入文件
         files.write(filePath, req.body.string());
+        alert('更新完成');
     }
-
+} else {
+    // 变例读取文件
+    for (i = 0; i <= jsFiles.length; i++) {
+        fileName = jsFiles[i];              // 文件名
+        filePath = dir + '/' + fileName;    // 文件路径
+        fileUrl = originUrl + fileName;     // 网络文件路径
+        var req = http.get(fileUrl);
+        if (req.statusCode != '200') {
+            toastLog('网络读取错误，可能文件不存在' + req.statusMessage)
+            sleep(800);
+        } else {
+            toastLog('更新完成 写入文件' + req.statusMessage)
+            // 写入文件
+            files.write(filePath, req.body.string());
+        }
+    }
+    alert('更新完成');
 }
-
-alert(fileName + '更新完成');
