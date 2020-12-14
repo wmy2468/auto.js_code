61e71228665d2469d0e18346c9a57562487ffec4
JJ-Company <JJ@linde.com>
2020-12-14 11:39:01 +0800

    a

--- 抢商品.js
+++ 抢商品.js
@@ -4,8 +4,8 @@ var func = require("func_list.js");
 // 入口统一为 收藏夹
 var selectedArr = [
     "京东茅台",
-    "苏宁茅台",
-    "天猫茅台"
+    "天猫茅台",
+    "苏宁茅台"
 ];
 
 //淘宝测试();
@@ -30,41 +30,34 @@ function main() {
 // ------------------------------------------------------
 
 // 到点点击
-function 苏宁茅台() {
-    var startTime, targetViewText;
-    var selActIdx = dialogs.select("选择启动", actNames);
-    if (selActIdx == -1) {
-        exit();
-    }
-    var actName = actNames[selActIdx];
-    switch (actName) {
-        case "每日11点10元KFC":
-            startTime = "11,00,00,000";
-            targetViewText = "【活动编号】30456";
-            break;
-        case "每日10点10元30天猫":
-            startTime = "10,00,00,000";
-            targetViewText = "【活动编号】23851";
-            break;
-        case "周末11点50元必胜客":
-            startTime = "11,00,00,000";
-            targetViewText = "【活动编号】26829";
-            break;
-        case "周末11点50元海底捞":
-            startTime = "11,00,00,000";
-            targetViewText = "【活动编号】26951";
-            break;
-    }
+function 京东茅台() {
+    // 只有购物车抢购模式
+    toastLog("请选择到预约或收藏界面, 如有提示已到达等待页面请重启");
 
-    var appName = "阳光惠生活";
-    var timeArea = "北京时间";
+    var appName = "京东"
+    var timeArea = "京东时间";
+    var startTime = "09,59,59,666";
+    var targetViewText = "贵州茅台酒";;
+    // 启动APP
     launchApp(appName);
-    // 等待进入指定页面
-    text(targetViewText).findOne();
-    toastLog("已到达指定页面，等待");
-    //   定位元素
+    // 等待用户选择到指定页面
+    while (text("我的预约").findOnce() || id("com.jd.lib.favourites.feature:id/favo_goods_tab").text("商品").findOnce()) {
+        sleep(800);
+    }
+    toastLog("已到达等待页面");
+    // 等待时间到达
     func.getTimeDiff(timeArea, startTime);
-    func.sClick(className("android.view.View").text("确认购买").findOne());
+    // 循环点击元素
+    // 点击商品
+    func.sClick(text(targetViewText).findOne());
+    // 点击立即抢购
+    func.sClick(text("立即抢购").findOne());
+    // 点击提交订单
+    while (1) {
+        func.sClick(text("提交订单").findOnce());
+        sleep(300);
+    }
+    // 提示结束
     toastLog("结束");
     sleep(800);
 }
@@ -73,31 +66,23 @@ function 天猫茅台() {
     // 只有购物车抢购模式
     var appName = "手机淘宝"
     var timeArea = "淘宝时间";
-    var startTimes = ["19,00,00,000", "20,00,00,000"];
+    var startTimes = ["18,59,59,666", "19,59,59,666"];
     var startTime = dialogsWin(startTimes);
     var targetViewText = "结算(1)";;
     // 启动APP
     launchApp(appName);
     // 等待用户选择到指定页面
     text(targetViewText).findOne();
+    toastLog("已到达等待页面");
     // 等待时间到达
-    // func.getTimeDiff(timeArea, startTime);
+    func.getTimeDiff(timeArea, startTime);
     // 循环点击元素
     var sureOrder, submitOrder;     // 确认订单，提交订单
-    while (1) {
-        // 点击结算
-        func.sClick(className("android.widget.TextView").id("button_cart_charge").text("结算(1)").findOne());
-        sureOrder = className("android.widget.TextView").id("btn_back").text("确认订单").findOne();
-        if (text("失效宝贝").findOnce() == null) {
-            func.sClick(sureOrder);     // 点击返回
-        }
-        else {
-            // 点击提交订单
-            var submitOrder = className("android.widget.TextView").text("提交订单").findOnce();
-            if (!submitOrder) {
-                func.sClick(submitOrder.parent());
-            }
-        }
+    func.sClick(className("android.widget.TextView").id("button_cart_charge").text("结算(1)").findOne());
+    var submitOrder = className("android.widget.TextView").text("提交订单").findOne();
+    while (text("支付宝账号").findOnce() == null) {
+        func.sClick(submitOrder.parent());
+        sleep(300);
     }
     // 提示结束
     toastLog("结束");
