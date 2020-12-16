bef416013cbf88a4cbf56c20b5d047aecad70fa0
JJ-Company <JJ@linde.com>
2020-12-16 11:18:02 +0800

    a

--- 抢券.js
+++ 抢券.js
@@ -33,6 +33,7 @@ function main() {
 
 // 到点点击
 function 光大活动() {
+    toastLog("到点点击");
     var startTime, targetViewText;
     var actNames = ["每日11点10元KFC", "每日10点10元30天猫", "周末11点50元海底捞", "周末11点50元必胜客"];
     var selActIdx = dialogs.select("选择启动", actNames);
@@ -74,6 +75,7 @@ function 光大活动() {
 
 // 等待页面加载
 function 交行5积分() {
+    toastLog("等待页面变化");
     var appName = "买单吧";
     launchApp(appName);
     // 等待进入指定页面
@@ -107,21 +109,17 @@ function 中信活动() {
     var couDes;    // 券描述
     switch (actName) {
         case "每日10点9积分兑换":
+            toastLog("等待页面变化");
             startTime = "09,59,45,000"
             couDes = ["星巴克中杯", "必胜客30元", "奈雪", "喜茶25元", "苏宁易购20元", "百果园20元", "京东支付券20元", "天猫20元", "美团外卖20元"];
-            targetViewText = func.dialogsWin(couDes);
-            // 找券
-            var couClick = textContains(couDes).findOne();
+            targetViewText = func.dialogsWin(couDes);               // 设置查找的文本
+            launchApp(appName);             // 启动APP
+            var couClick = textContains(couDes).findOne();          // 找券
             toastLog("已到找到对应券位置，提前15秒自动进入");
-            // 等待到15秒的时候再进入
-            func.getTimeDiff(timeArea, startTime);
-            // 点击标签
-            func.sClick(couClick);
-            // 等待页面变化
-            targetViewText = "价格: 1个权益+9个积分";
-            launchApp(appName);
-            // 等待进入指定页面
-            text(targetViewText).findOne();
+            func.getTimeDiff(timeArea, startTime);              // 等待到15秒的时候再进入
+            func.sClick(couClick);              // 点击标签
+            targetViewText = "价格: 1个权益+9个积分";               // 设置查找的文本
+            text(targetViewText).findOne();             // 等待进入指定页面
             toastLog("已到达指定页面，等待");
             //点击元素
             func.sClick(className("android.view.View").text("去兑换").findOne());
@@ -130,21 +128,15 @@ function 中信活动() {
             sleep(800);
             break;
         case "周三六11点5折券":
-            // 到点点击
-            startTime = "11,00,00,000";
-            // 券名称
-            couDes = ["必胜客100元代金券", "百果园50元代金券"];
-            // 设置查找的文本
-            targetViewText = func.dialogsWin(couDes);
-            launchApp(appName);
-            // 等待进入指定页面
-            className("android.view.View").text(targetViewText).findOne();
+            toastLog("到点点击");
+            startTime = "11,00,00,000";             // 设置时间点
+            couDes = ["必胜客100元代金券", "百果园50元代金券"];             // 券名称
+            targetViewText = func.dialogsWin(couDes);               // 设置查找的文本
+            launchApp(appName);             // 启动APP
+            className("android.view.View").text(targetViewText).findOne();              // 等待进入指定页面
             toastLog("已到达指定页面，等待");
-            // 等待时间
-            func.getTimeDiff(timeArea, startTime);
-            // 点击元素
-            func.sClick(className("android.view.View").text(targetViewText).findOne());
-            toastLog("点击商品");
+            func.getTimeDiff(timeArea, startTime);              // 等待时间
+            func.sClick(className("android.view.View").text(targetViewText).findOne());             // 点击元素
             // 点击元素
             func.sClick(className("android.widget.Button").text("立即购买").findOne());
             func.sClick(className("android.view.View").text("确认").findOne());
@@ -156,6 +148,7 @@ function 中信活动() {
 
 // 到点点击
 function 掌上星巴克() {
+    toastLog("到点点击");
     var appName = "掌上生活";
     var startTime = "10,00,00,100";
     var timeArea = "北京时间";
@@ -176,6 +169,7 @@ function 掌上星巴克() {
 
 // 等待页面变价
 function 京东腾讯月() {
+    toastLog("等待页面变化");
     var appName = "京东金融";
     launchApp(appName);
     // 等待进入指定页面    
--- 抢商品.js
+++ 抢商品.js
@@ -12,7 +12,6 @@ main();
 
 //---------------配置区域-----------------
 function main() {
-
     var selectIndex = dialogs.select("先打开抢购页面,再启动", selectedArr);
     if (selectIndex == -1) {
         exit();
@@ -30,26 +29,22 @@ function main() {
 
 // 到点点击
 function 京东茅台() {
-    // 只有购物车抢购模式
+    // 等待页面变价模式
     toastLog("请选择到预约或收藏界面, 如有提示已到达等待页面请重启");
 
     var appName = "京东"
     var timeArea = "京东时间";
     var startTime = "09,59,50,000";
     var targetViewText = "贵州茅台酒";
-    // 启动APP
-    launchApp(appName);
+    launchApp(appName);             // 启动APP
     // 等待用户选择到指定页面
     while (text("我的预约").findOnce() || id("com.jd.lib.favourites.feature:id/favo_goods_tab").text("商品").findOnce()) {
         sleep(800);
     }
     toastLog("已到达等待页面");
-    // 等待时间到达
-    func.getTimeDiff(timeArea, startTime);
-    // 点击商品进入
-    func.sClick(textContains(targetViewText).findOne());
-    // 循环点击元素 等待页面变价
-    func.sClick(text("立即抢购").findOne());
+    func.getTimeDiff(timeArea, startTime);              // 等待时间到达
+    func.sClick(textContains(targetViewText).findOne());                // 点击商品进入
+    func.sClick(text("立即抢购").findOne());                // 循环点击元素 等待页面变价
     // 点击提交订单
     while (!func.sClick(text("提交订单").findOnce())) {
         // 点击立即抢购
@@ -73,16 +68,13 @@ function 天猫茅台() {
     // 只有购物车抢购模式
     var appName = "手机淘宝"
     var timeArea = "淘宝时间";
-    var startTime = "19,59,59,666";
+    var startTime = "19,59,59,555";
     var targetViewText = "结算(1)";;
-    // 启动APP
-    launchApp(appName);
-    // 等待用户选择到指定页面
-    text(targetViewText).findOne();
+    launchApp(appName);             // 启动APP
+    text(targetViewText).findOne();             // 等待用户选择到指定页面
     toastLog("已到达等待页面");
     var submitOrder;     // 提交订单
-    // 等待时间到达
-    func.getTimeDiff(timeArea, startTime);
+    func.getTimeDiff(timeArea, startTime);              // 等待时间到达
     // 循环点击元素
     func.sClick(className("android.widget.TextView").id("button_cart_charge").text("结算(1)").findOne());
     var submitOrder = className("android.widget.TextView").text("提交订单").findOne();
