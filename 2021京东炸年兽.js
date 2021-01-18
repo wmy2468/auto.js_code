f19c7b064c07b76d8f90ef0c85a5eefa143ead40
JJ-Company <JJ@linde.com>
2021-01-18 10:01:43 +0800

    a

--- 2021京东炸年兽.js
+++ 2021京东炸年兽.js
@@ -32,32 +32,15 @@ function monster() {
         unComplete = text('去完成').find();
         if (unComplete.nonEmpty()) {
             if (unComplete.length <= 1 || idx >= unComplete.length) { break; }
-            idxText = unComplete[idx].parent().parent().parent().child(0).child(1).text();
-            if (idxText.indexOf('去玩AR吃') == -1) {
-                // 如果有战队相关则+1
-                if (idxText.indexOf('战队') != -1) {
-                    idx = idx + 1;
-                    continue;
-                }
-
-                if (idx >= unComplete.length) { break; }
-                sleep(1500);
-                if (textContains('继续领红包').findOnce() != null) {
-                    textContains('继续领红包').findOnce().click();
-                    sleep(1500);
-                }
-                while (text('去完成').findOnce() != null) {
-                    unComplete[idx].click();
-                    sleep(1000);
-                }
-                if (idxText.indexOf('浏览可得') != -1) { textStr = '直接返回' }
-                else if (idxText.indexOf('秒可得') != -1) { textStr = '等待返回' }
-                else if (idxText.indexOf('成功开通得') != -1) { textStr = '会员' }
-                else if (idxText.indexOf('浏览5个商品可得') != -1) { textStr = '浏览' }
-                else if (idxText.indexOf('加购5个商品可得') != -1) { textStr = '加购' }
-            } else {
-                break;
+            idxText = unComplete[idx].parent().child(2).text();
+            sleep(1500);
+            while (text('去完成').findOnce() != null) {
+                unComplete[idx].click();
+                sleep(1000);
             }
+            if (idxText.indexOf('浏览可得') != -1) { textStr = '直接返回' }
+            else if (idxText.indexOf('秒可得') != -1) { textStr = '等待返回' }
+            else if (idxText.indexOf('浏览并加购5个商品可得') != -1) { textStr = '加购' }
             toastLog(textStr);
             after_click(textStr);
         } else {
@@ -92,7 +75,7 @@ function after_click(textStr) {
     }
     else if (textStr == '直接返回') {
         sleep(1000);
-        back_way();
+        back();
     }
     else if (textStr == '等待返回') {
         wait_complete();
@@ -119,55 +102,54 @@ function add_cart() {
         }                   //加购等待已完成 
         i = i + 1;
     }
-    while (className('android.view.View').textContains('签到').findOnce() == null) {
-        back_way();
-        sleep(2000);
-    }
+    back_way();
+    sleep(2000);
 }
 
 
-function view_list() {
-    i = 0;
-    while (text('已完成').findOnce() == null) {
-        idContains('view_').findOne();
-        //点击商品加购物车按钮
-        if (idContains('view_').findOnce() != null) {
-            idContains('view_').find()[i].click();
-            textContains('购物车').findOne();
-            sleep(1500);
-            back_way();
-            sleep(2500);
-        }
-        i = i + 1;
-    }
-    while (className('android.view.View').textContains('签到').findOnce() == null) {
-        back_way();
-        sleep(2000);
-    }
-}
+// function view_list() {
+//     i = 0;
+//     while (text('已完成').findOnce() == null) {
+//         idContains('view_').findOne();
+//         //点击商品加购物车按钮
+//         if (idContains('view_').findOnce() != null) {
+//             idContains('view_').find()[i].click();
+//             textContains('购物车').findOne();
+//             sleep(1500);
+//             back_way();
+//             sleep(2500);
+//         }
+//         i = i + 1;
+//     }
+//     while (className('android.view.View').textContains('签到').findOnce() == null) {
+//         back_way();
+//         sleep(2000);
+//     }
+// }
 
-function member_card() {
-    var count = 0;
-    //toastLog('会员卡');
-    sleep(3000);
-    while (text('去完成').findOnce() == null) {
-        if (count >= 4) {
-            back_way();
-            sleep(4000);
-        }
-        if (center_click(textContains('确认授权并加入').findOnce())) {
-            sleep(3000);
-            center_click(text('我知道了').findOnce())
-        }
-        count = count + 1;
-        sleep(3000);
-    }
-}
+// function member_card() {
+//     var count = 0;
+//     //toastLog('会员卡');
+//     sleep(3000);
+//     while (text('去完成').findOnce() == null) {
+//         if (count >= 4) {
+//             back_way();
+//             sleep(4000);
+//         }
+//         if (center_click(textContains('确认授权并加入').findOnce())) {
+//             sleep(3000);
+//             center_click(text('我知道了').findOnce())
+//         }
+//         count = count + 1;
+//         sleep(3000);
+//     }
+// }
 
 function wait_complete() {
     //等待恭喜完成
-    textContains('恭喜完成').findOne();
-    back_way();
+    var backNow = textContains('立刻返回').findOne();
+    sleep(1000);
+    func.sClick(backNow);
 }
 
 // -------------通用部分--------------------
--- test.js
+++ test.js
@@ -1,5 +1,3 @@
-var idxText;
-text('去完成').findOne();
-unComplete = text('去完成').find();
-idxText = unComplete[0].parent().parent().parent().child(0).child(1).text();
-toastLog(idxText);
+var backNow = textContains('立刻返回').findOne();
+sleep(1000);
+func.sClick(backNow);
