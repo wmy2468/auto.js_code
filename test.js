4ef2597f1e7bbded7c09cd2ac0e778b8f36e8097
JJ-Company <JJ@linde.com>
2021-01-18 09:40:59 +0800

    a

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 var idxText;
 text('去完成').findOne();
 unComplete = text('去完成').find();
-idxText = unComplete[idx].parent().parent().parent().child(0).child(1).text();
+idxText = unComplete[0].parent().parent().parent().child(0).child(1).text();
 toastLog(idxText);
