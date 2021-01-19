11464b09268ce63f16cda0da0e92d753644a44b6
JJ-Company <JJ@linde.com>
2021-01-19 08:17:55 +0800

    1

--- test.js
+++ test.js
@@ -3,7 +3,7 @@ var func = require("func_list.js");
 
 //var backNow = textContains('立刻返回').findOne();
 
-func.sClick(className('android.view.View').text("我知道了").findOnce().parent().parent().parent().child(3));
+func.sClick(className('android.view.View').text("立即抽奖").findOnce().parent().parent().parent().child(3));
 
 // var backNow = descContains('立刻返回').findOne();
 // sleep(1000);
