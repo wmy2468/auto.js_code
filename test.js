6af79370c927e4e7049efe4c2e752e5cfbb7d118
JJ-Company <JJ@linde.com>
2021-01-19 14:44:30 +0800

    test

--- test.js
+++ test.js
@@ -3,9 +3,10 @@ var func = require("func_list.js");
 
 //var backNow = textContains('立刻返回').findOne();
 
-var popUp = text("立即抽奖").findOnce();
+var popUp = text("立即抽奖").findOne();
 var popUpLen = popUp.parent().parent().parent().childCount();
-func.sClick(popUp.child(popUpLen - 1));
+toastLog(popUpLen);
+func.sClick(popUp.parent().parent().parent().child(popUpLen - 1));
 
 // var backNow = descContains('立刻返回').findOne();
 // sleep(1000);
