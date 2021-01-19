02396410b4097f00c6a37d7a66e3e4adc8b7386c
JJ-Company <JJ@linde.com>
2021-01-19 13:26:14 +0800

    w

--- 2021京东炸年兽.js
+++ 2021京东炸年兽.js
@@ -7,15 +7,15 @@ var appNumbers = 1;
 main();
 
 function main() {
-    var selectedArr = ['第一个APP', '第二个APP']
+    var selectedArr = ["第一个APP", "第二个APP"]
     var selNum;
     if (device.brand == "xiaomi") {
         selNum = func.dialogsWin(selectedArr);
         switch (selNum) {
-            case 第一个APP:
+            case "第一个APP":
                 appNumbers = 1;
                 break;
-            case 第二个APP:
+            case "第二个APP":
                 appNumbers = 2;
                 break;
         }
@@ -24,16 +24,16 @@ function main() {
 
     kouling();
     monster();
-    alert('已完成！');
+    alert("已完成！");
 }
 
 function kouling() {
-    setClip('18.0复制整段话 http:/J8KywSROEN3jkp幫皒助屴，1起炸哖獸分10億吧！>>ㄣ￥mDmQ3EB2Rb%da開(倞A崬pp）');
-    func.sClick(id('com.jingdong.app.mall:id/bci').text('立即查看').findOne());
+    setClip("18.0复制整段话 http:/J8KywSROEN3jkp幫皒助屴，1起炸哖獸分10億吧！>>ㄣ￥mDmQ3EB2Rb%da開(倞A崬pp）");
+    func.sClick(id("com.jingdong.app.mall:id/bci").text("立即查看").findOne());
     var popUp, popUpLen;
     toastLog("等待任务页面加载");
     while (text("每邀1个好友可得10000爆竹").findOnce() == null) {
-        func.sClick(text('集爆竹').findOnce());
+        func.sClick(text("集爆竹").findOnce());
         sleep(2500);
         // 关闭弹窗
         func.sClick(text("我知道了").findOnce());
@@ -61,22 +61,22 @@ function monster() {
     var idx = 1;
     var idxText, unComplete, textStr;
     while (true) {
-        textStr = '';
+        textStr = "";
         text("每邀1个好友可得10000爆竹").findOne();
-        unComplete = text('去完成').find();
+        unComplete = text("去完成").find();
         if (unComplete.nonEmpty()) {
             log("去完成长度：" + unComplete.length);
             if (unComplete.length <= 1 || idx >= unComplete.length) { break; }
             idxText = unComplete[idx].parent().child(2).text();
             // 判断文本的描述是属于哪种
-            if (idxText.indexOf('浏览可得') != -1) { textStr = '直接返回' }
-            else if (idxText.indexOf('秒可得') != -1) { textStr = '等待返回' }
-            else if (idxText.indexOf('浏览并加购5个商品可得') != -1) { textStr = '加购' }
-            else if (idxText.indexOf('成功入会可得') != -1) { textStr = '会员' }
-            else if (idxText.indexOf('浏览并关注频道可得') != -1) { textStr = '直接返回' }
+            if (idxText.indexOf("浏览可得") != -1) { textStr = "直接返回" }
+            else if (idxText.indexOf("秒可得") != -1) { textStr = "等待返回" }
+            else if (idxText.indexOf("浏览并加购5个商品可得") != -1) { textStr = "加购" }
+            else if (idxText.indexOf("成功入会可得") != -1) { textStr = "会员" }
+            else if (idxText.indexOf("浏览并关注频道可得") != -1) { textStr = "直接返回" }
             toastLog(textStr);
             sleep(1500);
-            while (text('去完成').findOnce() != null) {
+            while (text("去完成").findOnce() != null) {
                 func.cClick(unComplete[idx]);
                 sleep(1000);
             }
@@ -91,24 +91,24 @@ function monster() {
 function after_click(textStr) {
     toastLog("已点击去完成，等待下一步...");
     sleep(3000);
-    //var city_player = className('android.webkit.WebView').text('京喜城市玩家').findOnce();
-    //var viewList = text('浏览以下5个商品').depth(17).findOnce(); //恭喜完成
-    //var addCart = textContains('点击加购以下').findOnce();  //idContains(str)
+    //var city_player = className("android.webkit.WebView").text("京喜城市玩家").findOnce();
+    //var viewList = text("浏览以下5个商品").depth(17).findOnce(); //恭喜完成
+    //var addCart = textContains("点击加购以下").findOnce();  //idContains(str)
 
-    if (textStr == '加购') {
+    if (textStr == "加购") {
         add_cart();
     }
-    else if (textStr == '浏览') {
+    else if (textStr == "浏览") {
         view_list();
     }
-    else if (textStr == '直接返回') {
+    else if (textStr == "直接返回") {
         sleep(2500);
         back();
     }
-    else if (textStr == '等待返回') {
+    else if (textStr == "等待返回") {
         wait_complete();
     }
-    else if (textStr == '会员') {
+    else if (textStr == "会员") {
         member_card();
     }
     else {
@@ -126,9 +126,9 @@ function add_cart() {
             break;
         }
         //点击商品加购物车按钮
-        if (idContains('jmdd-react-smash').findOnce() != null) {
-            carts = idContains('jmdd-react-smash').find()[i].click();
-            //if (carts.child(0).text() != '已加购') {
+        if (idContains("jmdd-react-smash").findOnce() != null) {
+            carts = idContains("jmdd-react-smash").find()[i].click();
+            //if (carts.child(0).text() != "已加购") {
             //}
             while (!(textContains("购物车").findOnce() || textContains("店铺").findOnce())) {
                 sleep(1000);
@@ -222,17 +222,17 @@ function check_current_pkg(app_name) {
 
 function member_card() {
     var count = 0;
-    //toastLog('会员卡');
+    //toastLog("会员卡");
     sleep(3000);
-    while (text('去完成').findOnce() == null) {
+    while (text("去完成").findOnce() == null) {
         if (count >= 4) {
-            func.sClick(className('android.view.View').text("295042cd75137e90").findOnce());
+            func.sClick(className("android.view.View").text("295042cd75137e90").findOnce());
             back();
             sleep(2000);
         }
-        if (func.cClick(textContains('确认授权并加入').findOnce())) {
+        if (func.cClick(textContains("确认授权并加入").findOnce())) {
             sleep(3000);
-            func.cClick(text('我知道了').findOnce())
+            func.cClick(text("我知道了").findOnce())
         }
         count = count + 1;
         toastLog("未找到会员...等待返回")
@@ -241,19 +241,19 @@ function member_card() {
 }
 // function view_list() {
 //     i = 0;
-//     while (text('已完成').findOnce() == null) {
-//         idContains('view_').findOne();
+//     while (text("已完成").findOnce() == null) {
+//         idContains("view_").findOne();
 //         //点击商品加购物车按钮
-//         if (idContains('view_').findOnce() != null) {
-//             idContains('view_').find()[i].click();
-//             textContains('购物车').findOne();
+//         if (idContains("view_").findOnce() != null) {
+//             idContains("view_").find()[i].click();
+//             textContains("购物车").findOne();
 //             sleep(1500);
 //             back_way();
 //             sleep(2500);
 //         }
 //         i = i + 1;
 //     }
-//     while (className('android.view.View').textContains('签到').findOnce() == null) {
+//     while (className("android.view.View").textContains("签到").findOnce() == null) {
 //         back_way();
 //         sleep(2000);
 //     }
