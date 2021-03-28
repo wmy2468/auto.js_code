714bf9da1ff8efbad7e63d5c1b373742f55d3d7f
JJ-LAPTOP <xuiewy@163.com>
2021-03-28 13:22:57 +0800

    a

--- test.js
+++ test.js
@@ -1,12 +1,12 @@
 // 导入模块
 var func = require("func_list.js");
-浦发银行();
+
 while (true) {
     func.sClick(text("全部").findOnce());
     func.sClick(text("待付款").findOnce());
     func.sClick(text("去支付").findOnce());
     if (func.sClick(id("com.jd.lib.cashier.feature:id/cf").findOnce())) {
-        while (func.sClick(text("返回商家").findOnce())) {
+        while (!func.sClick(text("完成").findOnce())) {
             func.sClick(text("立即支付").findOnce());
             func.sClick(text("确认支付").findOnce());
         }
