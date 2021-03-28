4c4db1424aa95ebcb47fd40b9b52d48752f03d18
JJ-LAPTOP <xuiewy@163.com>
2021-03-28 13:25:03 +0800

    a

--- test.js
+++ test.js
@@ -1,15 +1,17 @@
 // 导入模块
 var func = require("func_list.js");
 
+func.toApp("京东");
 while (true) {
     func.sClick(text("全部").findOnce());
     func.sClick(text("待付款").findOnce());
     func.sClick(text("去支付").findOnce());
     if (func.sClick(id("com.jd.lib.cashier.feature:id/cf").findOnce())) {
-        while (!func.sClick(text("完成").findOnce())) {
+        while (!func.sClick(text("返回商家").findOnce())) {
             func.sClick(text("立即支付").findOnce());
             func.sClick(text("确认支付").findOnce());
         }
+        text("完成").findOne()
     }
     if (func.sClick(text("立即抽奖").findOnce())) {
         text("恭喜你").findOne();
