895c8e876264bd4ea7eccdc1b806b788c50f4786
JJ-LAPTOP <xuiewy@163.com>
2021-03-28 13:10:37 +0800

    a

--- test.js
+++ test.js
@@ -1,39 +1,15 @@
 // 导入模块
 var func = require("func_list.js");
 浦发银行();
-
-function 浦发银行() {
-    var appName = "浦发银行";
-    //closeApp(appName);
-    func.toApp(appName);
-    while (text("首页").findOnce() == null) {
-        func.passAd();
+while (true) {
+    func.sClick(text("全部").findOnce());
+    func.sClick(text("待付款").findOnce());
+    func.sClick(text("去支付").findOnce());
+    func.sClick(id("com.jd.lib.cashier.feature:id/cf").findOnce());
+    func.sClick(text("立即支付").findOnce());
+    func.sClick(text("确认支付").findOnce());
+    func.sClick(text("返回商家").findOnce());
+    if (func.sClick(text("立即抽奖").findOnce())) {
+        text("恭喜你").findOne();
     }
-    sleep(1800);
-    func.sClick(id("radio_button5").text("我的").findOnce());
-    // 等待我的页面加载
-    text("日历提醒").findOne();
-    // 签到按钮
-    while (text("金豆").findOnce() == null) {
-        func.toAutojs();
-        func.toApp(appName);
-        sleep(3000);
-    }
-    sleep(800);
-    while (text("开启签到提醒").findOnce() == null) {
-        func.sClick(text("金豆").findOnce());
-        sleep(800);
-        if (text("切换登录方式").findOnce() || text("更多快捷方式登录").findOnce()) {
-            sleep(500);
-            func.gesture_pwd(appName);
-            sleep(1000);
-        }
-    }
-    sleep(1000);
-    while (textContains("连续签到").find().length > 2) {
-        func.sClick(textContains("连续签到").findOnce());
-
-    }
-    toastLog(appName + "已签到");
-    sleep(1000);
 }
