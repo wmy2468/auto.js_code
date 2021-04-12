f62c2e6f1f06dd0cac5387a98a179618c42bb58f
JJcompany <JJ@company.com>
2021-04-12 14:39:35 +0800

    a

--- 银行签到.js
+++ 银行签到.js
@@ -434,9 +434,10 @@ function 招商银行() {
     setText(0, "刮刮乐");
     sleep(500);
     KeyCode("KEYCODE_ENTER");
+    func.sClick(text("招牌便民刮刮乐").findOne());
     sleep(500);
     while (text("周日").findOnce() == null) {
-        func.sClick(text("招牌便民刮刮乐").findOne());
+
         if (id("ivBigHeadImage").findOnce() != null) {
             sleep(500);
             func.gesture_pwd(appName);
