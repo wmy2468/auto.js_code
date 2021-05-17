7826b359acb25297d18f2eb8bade2d5226a86f72
JJcompany <JJ@company.com>
2021-05-17 16:44:22 +0800

    d

--- test.js
+++ test.js
@@ -4,58 +4,45 @@ var func = require("func_list.js");
 main();
 
 function main() {
-    pufa_CXK();
     pufa_XYK();
     //gonghang_XYK();
     //youchu_CXK();
 
     youchu_XYK();
     //huaxia_XYK();
-    jiaohang_XYK();
     alert("已完成.");
 }
 
-// 金豆签到
-function pufa_CXK() {
-    var appName = "cn.com.spdb.mobilebank.per";
+// 买单吧
+function jiaohang_XYK() {
+    var appName = "com.bankcomm.maidanba";
     //closeApp(appName);
     func.toPackage(appName);
-    while (text("首页").findOnce() == null) {
+    while (className("TextView").id("tv_title").text("我的").findOnce() == null) {
         func.passAd();
+        func.sClick(id("ivADClose").findOnce());
     }
-    sleep(1800);
-    func.sClick(id("radio_button5").text("我的").findOnce());
-    // 等待我的页面加载
-    text("日历提醒").findOne();
-    // 签到按钮
-    while (text("金豆").findOnce() == null) {
-        func.toAutojs();
-        func.toApp(appName);
-        sleep(3000);
-    }
-    sleep(800);
-    while (text("开启签到提醒").findOnce() == null) {
-        func.sClick(text("金豆").findOnce());
-        sleep(800);
-        if (text("切换登录方式").findOnce() || text("更多快捷方式登录").findOnce()) {
+    func.sClick(text("我的").findOne());
+    sleep(2000);
+    while (id("rl_title_white").findOnce() == null) {
+        func.sClick(id("com.bankcomm.maidanba:id/tv_sign").text("每日签到").findOnce());
+        sleep(1000);
+        func.sClick(idContains("lose").findOnce());
+        log(appName + "等待登录");
+        sleep(1500);
+        if (text("手势登录").findOnce() != null) {
             sleep(500);
             func.gesture_pwd(appName);
             sleep(1000);
         }
     }
     sleep(1000);
-    var signs = textStartsWith("+").find();
-    try {
-        if (signs.length >= 2) {
-            for (var i = 0; i < signs.length; i++) {
-                func.sClick(signs[i]);
-            }
-        }
-    }
-    catch (e) {
-        toastLog("未找到多余的连续签到");
-        sleep(2000);
+    while (!(text("客官明天再来呦").findOnce() != null || text("完成").findOnce() != null)) {
+        func.sClick(id("bt_signin").text("签到").findOnce());
+        func.sClick(id("com.bankcomm.maidanba:id/bt_signin").text("抽奖").findOnce());
+        sleep(1000);
+        func.sClick(id("com.bankcomm.maidanba:id/bt_welfare_lottery").text("去抽奖").findOnce());
     }
     toastLog(appName + "已签到");
-    sleep(3000);
+    sleep(1000);
 }
