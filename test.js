912392a74f7436baddf268a966b5032f7058ac8c
JJ Chen <mw03251214@163.com>
2021-05-17 17:28:12 +0800

    d

--- test.js
+++ test.js
@@ -4,7 +4,6 @@ var func = require("func_list.js");
 main();
 
 function main() {
-    pufa_XYK();
     //gonghang_XYK();
     //youchu_CXK();
 
@@ -12,32 +11,33 @@ function main() {
     alert("已完成.");
 }
 
-function youchu_XYK() {
-    var appName = "com.yitong.mbank.psbc.creditcard";
+function pufa_XYK() {
+    var appName = "com.spdbcc.app";
+    //closeApp(appName);
     func.toPackage(appName);
     while (text("我的").findOnce() == null) {
         func.passAd();
-        var Continue = className("TextView").text("继续使用").findOnce();
-        if (Continue != null) {
-            func.sClick(Continue);
-        }
     }
-    sleep(800);
-    func.sClick(className("TextView").text("精选").findOne());
-    sleep(800);
+    func.sClick(text("我的").findOne());
     // 等待我的页面加载
-    var hotAct = text("热门活动").findOne().parent().parent();
-    sleep(800);
-    func.sClick(hotAct.child(6));
-    while (!(text("明天再来哦").findOnce() != null || textContains("恭喜获得").findOnce() != null)) {
-        func.sClick(text("立即签到").findOnce());
-        sleep(800);
-        if (text("忘记手势密码").findOnce() != null) {
-            sleep(500);
-            func.gesture_pwd(appName);
-            sleep(1000);
-        }
+    text("我的订单").findOne();
+    while (text("签到").findOnce() == null) {
+        func.toAutojs();
+        func.toApp(appName);
+        sleep(3000);
     }
+    func.sClick(text("签到").findOne());
+    // 输入手势密码
+    textContains("手势密码").findOne();
+    sleep(500);
+    func.gesture_pwd(appName);
+    sleep(1000);
+    // 等待签到页面加载
+    text("每日签到").findOne();
+    sleep(1000);
+    var waitSign = text("待签到").findOne();
+    sleep(1000);
+    func.sClick(waitSign.parent().parent().parent().parent().child(4));
     toastLog(appName + "已签到");
     sleep(1000);
 }
