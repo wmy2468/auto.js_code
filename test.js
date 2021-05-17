debfc62723970387a98a86bdfc6b1c7bd667ac7a
JJcompany <JJ@company.com>
2021-05-17 16:41:28 +0800

    c

--- test.js
+++ test.js
@@ -4,9 +4,6 @@ var func = require("func_list.js");
 main();
 
 function main() {
-    // zhonghang_XYK();
-    nongye_CXK();
-    jd_sign();
     YunShaofu();
     pufa_CXK();
     pufa_XYK();
@@ -19,39 +16,41 @@ function main() {
     alert("已完成.");
 }
 
-// 小豆
-function nongye_CXK() {
-    var appName = "com.android.bankabc";
+function YunShaofu() {
+    var appName = "com.unionpay";
     //closeApp(appName);
     func.toPackage(appName);
-    lineBtn = className("android.widget.LinearLayout").id("alphaTabsIndicator").findOnce();
-    while (lineBtn == null) {
-        lineBtn = className("android.widget.LinearLayout").id("alphaTabsIndicator").findOnce();
+    while (className("TextView").text("我 的").findOnce() == null) {
         sleep(1000);
     }
-    sleep(1000);
-    //点击我的按钮
-    func.sClick(lineBtn.child(4));
-    sleep(1200);
-    // 签到按钮
-    func.cClick(id("tv_my_haidou_unlogin").text("小豆").findOne());
-    //toastLog("我的已点击");
-    while (textContains("小豆秒杀").findOnce() == null) {
-        if (text("切换登录方式").findOnce() != null) {
-            //toastLog("滑动手势");
-            sleep(500);
-            func.gesture_pwd(appName);
-            sleep(2000);
+    sleep(1500);
+    func.sClick(className("TextView").text("首 页").findOnce());
+    //点击签到按钮
+    func.sClick(id("com.unionpay:id/frog_float_notgif").findOne());
+    // 等待签到页面加载
+    textContains("连续签到").findOne();
+
+    if (text("今日已签到").findOnce() == null) {
+        func.sClick(text("立即签到").findOnce());
+        sleep(1500);
+        /*
+        if (text("去抽奖").findOnce() != null) {
+            func.sClick(text("去抽奖").findOnce());
+            var area, areaP;
+            while (1) {
+                try {
+                    area = id("com.unionpay:id/tv_title").text("签到抽奖专区").findOnce();
+                    areaP = area.parent();
+                    sleep(2000);
+                    func.cClick(areaP.parent().child(2).child(0).child(0).child(0).child(0).child(0).child(3));
+                    idContains("resultBtn").findOnce();
+                    break;
+                } catch (err) {
+                    sleep(2500);
+                }
+            }
         }
-    }
-    //toastLog("找签到");
-    while (text("已经签到").findOnce() == null) {
-        func.sClick(text("收起").findOnce());
-        sleep(1200);
-        func.sClick(text("签到有礼").findOnce());
-        sleep(1200);
-        func.sClick(text("签到得豆").findOnce());
-        sleep(1200);
+        */
     }
     toastLog(appName + "已签到");
     sleep(1000);
