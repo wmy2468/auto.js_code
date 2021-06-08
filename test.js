1ac8b9e8769e8f92bea45748188f19cb2e4f28c6
JJcompany <JJ@company.com>
2021-06-08 10:45:11 +0800

    a

--- test.js
+++ test.js
@@ -2,29 +2,4 @@
 var func = require("func_list.js");
 //test on laptop
 
-var devBrand = device.brand;
-var 小米双开 = true;
-if (currentPackage() != "com.jingdong.app.mall") {
-    if (devBrand == 'HUAWEI') {
-        waitLog(8, '等待一会儿..跳转回JD');
-        func.toApp(appName);
-    } else if (devBrand == 'xiaomi') {
-        if (小米双开) {
-            waitLog(15, '等待一会儿..跳转回JD');
-            func.toAppMulti(appName, k);
-        } else {
-            waitLog(15, '等待一会儿..跳转回JD');
-            func.toApp(appName);
-        }
-    } else {
-        waitLog(15, '等待一会儿..跳转回JD');
-        func.toApp(appName);
-    }
-}
-
-function waitLog(cnt, textDetail) {
-    while (cnt--) {
-        toastLog(textDetail);
-        sleep(2000);
-    }
-}
+log(textContains('待会再来').findOnce().click());
