8ee3ab6290add04cb6adbf0d113862c38d352da8
JJcompany <JJ@company.com>
2021-06-24 14:01:31 +0800

    华为更新

--- 京东话费ZF.js
+++ 京东话费ZF.js
@@ -13,6 +13,7 @@ var textBar = "京东收银台"
 
 result = func.dialogsWin(selectArr);
 var cardEndNumber;
+var pwds = ['0', '8', '1', '5', '7', '3'];
 
 if (result == "weiXinn") {
     func.toApp("京东");
@@ -61,14 +62,11 @@ function hwzhifu() {
             func.sClick(text(textPay).findOnce());
             sleep(1250);
             func.sClick(text(textAll).findOnce());
-        } catch (e) {
-
-        }
+        } catch (e) { }
         if (text("重新加载").findOnce()) {
             back();
             sleep(500);
         }
-
         func.sClick(textContains("去支付").findOnce());
         if (text(textBar).findOnce() != null) {
             func.sClick(textContains("云闪付").findOne());
@@ -84,7 +82,6 @@ function hwzhifu() {
                 sleep(500);
             }
             text("选择付款方式").findOne();
-
             while (text("付款详情").findOnce() == null) {
                 sleep(500);
                 if (!func.cClick(text("[" + cardEndNumber + "]").findOnce())) {
@@ -94,27 +91,43 @@ function hwzhifu() {
                     sleep(2500);
                 }
             }
+            sleep(1200);
             func.sClick(text("确认付款").findOnce());
+
+            while (text("验证支付密码").findOnce() == null) { sleep(1000); }
+            toastLog("...等待输入MM...");
+            sleep(3000);
+            for (var i = 0; i < pwds.length; i++) {
+                func.sClick(text(pwds[i]).findOnce());
+                sleep(1200);
+            }
             text("支付成功").findOne();
-            sleep(1000);
+            toastLog("...支付完成...");
+            sleep(1200);
             back();
             log(text("查看订单").findOne());
             //if (func.sClick(text("立即抽奖").findOnce())) {
             if (text("立即抽奖").findOnce()) {
+                toastLog("找到抽奖，等待返回");
                 sleep(8000);
                 back();
                 // 待付款滑动栏
                 textContains(textPay).findOne();
+                cnt = 6;
+                while (cnt > 0) {
+                    cnt = cnt - 1;
+                    toastLog("...等待下一单...");
+                    sleep(4000);
+                }
             }
         }
     }
 }
 
+
 function weiXinn() {
-    var pwds = [0, 8, 1, 5, 7, 3];
     var cnt;
     while (true) {
-        cnt = 6;
         sleep(2000);
         try {
             func.sClick(text(textPay).findOnce());
@@ -140,14 +153,6 @@ function weiXinn() {
                 sleep(1000);
                 click(250, 1900);
             }
-            // while (func.cClick(text("返回商家").findOnce()) == false) {
-            //     func.sClick(text("立即支付").findOnce());
-            //     sleep(800);
-            //     func.sClick(text("继续支付").findOnce());
-            //     sleep(800);
-            //     func.sClick(text("确认支付").findOnce());
-            //     sleep(800);
-            // }
             // 尝试自动输入支付密码
             while (func.cClick(text("请输入支付密码").findOnce()) == false) {
                 func.sClick(text("立即支付").findOnce());
@@ -182,6 +187,7 @@ function weiXinn() {
             back();
             // 待付款滑动栏
             textContains(textPay).findOne();
+            cnt = 6;
             while (cnt > 0) {
                 cnt = cnt - 1;
                 toastLog("...等待下一单...");
@@ -197,17 +203,17 @@ function inputPwd(number) {
     var centerX = 540, centerY = 1952;
     var offsetX = 360, offsetY = 154;
     var nums = {
-        0: [centerX, centerY + offsetY + offsetY],      //x,y++
+        '0': [centerX, centerY + offsetY + offsetY],      //x,y++
 
-        1: [centerX - offsetX, centerY - offsetY],      //x-,y-
-        2: [centerX, centerY - offsetY],                //x,y-
-        3: [centerX + offsetX, centerY - offsetY],      //x+,y-
-        4: [centerX - offsetX, centerY],                //x-,y
-        5: [centerX, centerY],                          //x,y
-        6: [centerX + offsetX, centerY],                //x+,y
-        7: [centerX - offsetX, centerY + offsetY],      //x-,y+
-        8: [centerX, centerY + offsetY],                //x,y+
-        9: [centerX + offsetX, centerY + offsetY]       //x+,y+
+        '1': [centerX - offsetX, centerY - offsetY],      //x-,y-
+        '2': [centerX, centerY - offsetY],                //x,y-
+        '3': [centerX + offsetX, centerY - offsetY],      //x+,y-
+        '4': [centerX - offsetX, centerY],                //x-,y
+        '5': [centerX, centerY],                          //x,y
+        '6': [centerX + offsetX, centerY],                //x+,y
+        '7': [centerX - offsetX, centerY + offsetY],      //x-,y+
+        '8': [centerX, centerY + offsetY],                //x,y+
+        '9': [centerX + offsetX, centerY + offsetY]       //x+,y+
     }
     var point = nums[number];
     click(point[0], point[1]);
