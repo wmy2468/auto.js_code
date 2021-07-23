140b07ce010ee417fa32217675b34300c564e7f7
JJ-LAPTOP <xuiewy@163.com>
2021-07-24 07:30:01 +0800

    a

--- 抢券.js
+++ 抢券.js
@@ -81,6 +81,7 @@ function 京东() {
                 toastLog("请跳转到 京喜 购物车 页面，直到提示 已到达等待页面");
                 sleep(1000);
             }
+            toastLog("第一次已找到商品，准备进入商品详情");
             // 进入页面后，进入一次商品详情
             func.sClick(couClick);             // 点击元素
             // 延迟3秒后 返回
@@ -94,7 +95,6 @@ function 京东() {
             couClick = textContains(targetViewText).findOnce();
             while (true) {
                 couClick = textContains(targetViewText).findOnce();
-                func.sClick(text("购物车").findOnce());
                 toastLog("请跳转到 京喜 购物车 页面，直到提示 已到达等待页面");
                 if (couClick != null && text("单独购买").findOnce() == null) {
                     break;
