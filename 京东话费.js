739c3c3190ff1abb86aca2f4f5769296705a8e88
JJ-LAPTOP <xuiewy@163.com>
2021-08-07 12:05:13 +0800

    a

--- 京东话费.js
+++ 京东话费.js
@@ -61,6 +61,8 @@ function 话费支付() {
         }
         // 如果是荣耀8，先启动云闪付和钱包
         if (device.model == "FRD-AL00") {  
+            home();
+            sleep(1000);
             func.toApp("华为钱包");
             sleep(2000);
             home();
