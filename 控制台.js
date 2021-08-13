d8949f8d36f819149da8e09210cd5dbd85fb001e
JJcompany <JJ@company.com>
2021-08-13 11:16:56 +0800

    a

--- 控制台.js
+++ 控制台.js
@@ -22,6 +22,10 @@ var 控制台, 悬控移动和点击, 接码窗口;
       <TableRow>
         <horizontal>
           <button id='移动' layout_weight="1">移动</button>
+        </horizontal>
+      </TableRow>
+      <TableRow>
+        <horizontal>
           <button id='关闭' layout_weight="1">关闭</button>
         </horizontal>
       </TableRow>
