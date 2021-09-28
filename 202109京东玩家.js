fc1c6638e7c8ef6a908df536db656432e2ef03bb
JJcompany <JJ@company.com>
2021-09-28 10:55:04 +0800

    a

--- 202109京东玩家.js
+++ 202109京东玩家.js
@@ -456,10 +456,10 @@ function member_card() {
 		}
 	}
 	sleep(2200);
+	back_way();
+	sleep(1000);
 	func.sClick(desc('关闭页面').findOnce());
 	func.sClick(id('com.jingdong.app.mall:id/ge').findOnce());
-	sleep(1000);
-	back_way();
 	sleep(1500);
 }
 
