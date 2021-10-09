7d206e5773d48e5a42f09600bd424c1c54a6f772
JJcompany <JJ@company.com>
2021-10-09 10:49:54 +0800

    a

--- 时间显示.js
+++ 时间显示.js
@@ -36,6 +36,7 @@ if (selectIndex == "延迟测试") {
 
         stTimestamp = new Date();
         try {
+            http.__okhttp__.setTimeout(800);       // 设置超时2秒
             res = http.get("https://f.m.suning.com/api/ct.do");
             edTimestamp = new Date();
             resultStr = resultStr + area + "请求时差:" + (edTimestamp - stTimestamp) + "\n";
