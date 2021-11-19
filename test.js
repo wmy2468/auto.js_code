11bc51b6acd3efb6234cf60246620d79ffd3c4c9
JJcompany <JJ@company.com>
2021-11-19 08:40:54 +0800

    a

--- test.js
+++ test.js
@@ -62,8 +62,56 @@ var func = require("func_list.js");
 // for (var i = 0; i < signs.length; i++) {
 //     func.sClick(signs[i]);
 // }
-var checkText = "龙支付分会场";
-log(text(checkText).findOnce());
+
+
+浦发银行();
+
+// 浦发金豆签到
+function 浦发银行() {
+    var appName = "浦发银行";
+    //closeApp(appName);
+    func.toApp(appName);
+    while (text("我的账户").findOnce() == null) {
+        func.passAd();
+    }
+    sleep(1800);
+    while (text("查看收益/收支明细").findOnce() == null) {
+        func.sClick(idContains("radio_button5").findOnce());
+        sleep(3000);
+    }
+    // 签到按钮
+    while (text("金豆").findOnce() == null) {
+        func.toAutojs();
+        func.toApp(appName);
+        sleep(3000);
+    }
+    sleep(800);
+    while (text("收支").findOnce() == null) {
+        func.sClick(text("金豆").findOnce());
+        sleep(800);
+        if (text("切换登录方式").findOnce() || text("更多快捷方式登录").findOnce()) {
+            sleep(500);
+            func.gesture_pwd(appName);
+            sleep(1000);
+        }
+    }
+    sleep(1500);
+    scrollDown();
+    textStartsWith("+").findOne();
+    sleep(800);
+    var signs = textStartsWith("+").find();
+    try {
+        for (var i = 0; i < signs.length; i++) {
+            func.sClick(signs[i]);
+        }
+    }
+    catch (e) {
+        toastLog("未找到多余的连续签到");
+        sleep(2000);
+    }
+    toastLog(appName + "已签到");
+    sleep(3000);
+}
 
 function 品牌墙() {
     var find_text, find_object, find_object_index, find_object_parent;	// 定义查找的变量
