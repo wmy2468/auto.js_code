0cb79fd7aa1cd719db7e7db7cf2ae3dc12b7732d
JJcompany <JJ@company.com>
2021-12-17 08:55:54 +0800

    增加京东签到

--- scheme功能清单.js
+++ scheme功能清单.js
@@ -1,34 +1,36 @@
 // --------------url scheme--------------
-var 产品ID = "";
-产品ID = "http://coupon.m.jd.com/coupons/show.action?key=g7udi9d8e5260e8b7a8a76c0d01209e8&roleId=62130462";
+let 产品ID = "";
+let 产品ID = "http://coupon.m.jd.com/coupons/show.action?key=g7udi9d8e5260e8b7a8a76c0d01209e8&roleId=62130462";
 // 1. 跳转京喜APP，需要修改  => 产品ID
-url_京喜 = "openapp.jdpingou://virtual?params=%7B%22des%22%3A%22m%22%2C%22url%22%3A%22https%3A%2F%2Fitem.m.jd.com%2Fitem%2Fjxview%3Fsku%3D" + 产品ID + "%22%2C%22category%22%3A%22jump%22%7D"
+let url_京喜 = "openapp.jdpingou://virtual?params=%7B%22des%22%3A%22m%22%2C%22url%22%3A%22https%3A%2F%2Fitem.m.jd.com%2Fitem%2Fjxview%3Fsku%3D" + 产品ID + "%22%2C%22category%22%3A%22jump%22%7D"
 // 跳转京东APP的商品详情
-url_京东 = "openapp.jdMobile://virtual?params=%7B%22sourceValue%22:%220_productDetail_97%22,%22des%22:%22productDetail%22,%22skuId%22:%22" + 产品ID + "%22,%22category%22:%22jump%22,%22sourceType%22:%22PCUBE_CHANNEL%22%7D";
+let url_京东 = "openapp.jdMobile://virtual?params=%7B%22sourceValue%22:%220_productDetail_97%22,%22des%22:%22productDetail%22,%22skuId%22:%22" + 产品ID + "%22,%22category%22:%22jump%22,%22sourceType%22:%22PCUBE_CHANNEL%22%7D";
 
 券链接 = ""
-url_京喜_券页面 = "openapp.jdpingou://virtual?params=%7B%22des%22%3A%22m%22%2C%22url%22%3A%22" + 券链接 + "%22%2C%22category%22%3A%22jump%22%7D"
+let url_京喜_券页面 = "openapp.jdpingou://virtual?params=%7B%22des%22%3A%22m%22%2C%22url%22%3A%22" + 券链接 + "%22%2C%22category%22%3A%22jump%22%7D"
 
-// var url_工行小象 = "com.icbc.androidclient://startType=PORTALINJECT&menuId=xiaoxiangleyuan&shareCurrentUUID=";
-var url_工行小象 = "com.icbc.androidclient://startType=PORTALINJECT&menuId=taskCenter&injectParams=dGFyZ2V0PWVsZmxk&shareCurrentUUID="
+// let url_工行小象 = "com.icbc.androidclient://startType=PORTALINJECT&menuId=xiaoxiangleyuan&shareCurrentUUID=";
+let url_工行小象 = "com.icbc.androidclient://startType=PORTALINJECT&menuId=taskCenter&injectParams=dGFyZ2V0PWVsZmxk&shareCurrentUUID="
 
-url_农行小豆 = "bankabc://%7B%22method%22%3A%22jumpToSharedProduct%22%2C%22param%22%3A%22rR4uOmAzpF49gqwDYQiLp20AltfnLciJg3Fyp5ijEIlD6KSfPdLMNyKsM8JboO6MwU4dRe9KEsPXqC4shEX19X6hEiWyiILqbgLFXv9xJ5Jc7WP8cgtQBKyWQwTlznpR47%2BlnPSHUcgGQwprcCrZljQQsb3H9RhiJ2D2qeBt4JJz84Yh2iQ9R1lu%2FY%2BWKtaP25m0LbNLiCBYzuVXpAI%2BZfQKjVDNu72M0bgLPJtM1yg7oAXVGsadNuQMbKRz0XWTmkZzKVNYYupr4XqG08l6VvoOh1qETuzMO5mSCup%2FrhpJbwn4v5yYWC68q2FmK6K8YXpHZRtZyVIQwKrZKYjGCZ%2BdeHNIQKJe2plRNjawvy1QfB%2FYEIxcT68HT63j3KJK0%2FxlZSrAvT9cbSKRHkxleVMdKn8uj8HVMWs8l1DMdrLJK0tNFerEfKnSptOj4bSiQ6kvE4M2fMWrUzVIDPYLCHe2xvp9kZOZufXgyE5wze30A6S1HhYbMbNNCql08lmP0wQ3l%2Fp8fBF6hmgoQ73vZPphwCEOQxonP7IzQJC9%2Bl03cbmmPDij%2BBHrrczU55456whyF167TTNstajIJ4rERfcYdlkv3VOQEaXo%2BsUUrdXbI6wjb9vErff5hUgaW2%2FMl%2FZjNZthSCybk58RIUT3ndyGTtBSg%2B3hP4C4%2FvzRl3TXL0yiIELKVkzrELbYENRqWib%2B5aGXN1a54ll48VdKQiJFSZhEfYp27AW49Qxe7epmorgOuUBd76FZwMZCR%2Bg1QRnSS21%2F0PRVHSvWj3BDeST7nIue51s83rsZb9rkrE52ADZNwBV5mFrcRYyQoaKe%22%7D"
+let url_农行小豆 = "bankabc://%7B%22method%22%3A%22jumpToSharedProduct%22%2C%22param%22%3A%22rR4uOmAzpF49gqwDYQiLp20AltfnLciJg3Fyp5ijEIlD6KSfPdLMNyKsM8JboO6MwU4dRe9KEsPXqC4shEX19X6hEiWyiILqbgLFXv9xJ5Jc7WP8cgtQBKyWQwTlznpR47%2BlnPSHUcgGQwprcCrZljQQsb3H9RhiJ2D2qeBt4JJz84Yh2iQ9R1lu%2FY%2BWKtaP25m0LbNLiCBYzuVXpAI%2BZfQKjVDNu72M0bgLPJtM1yg7oAXVGsadNuQMbKRz0XWTmkZzKVNYYupr4XqG08l6VvoOh1qETuzMO5mSCup%2FrhpJbwn4v5yYWC68q2FmK6K8YXpHZRtZyVIQwKrZKYjGCZ%2BdeHNIQKJe2plRNjawvy1QfB%2FYEIxcT68HT63j3KJK0%2FxlZSrAvT9cbSKRHkxleVMdKn8uj8HVMWs8l1DMdrLJK0tNFerEfKnSptOj4bSiQ6kvE4M2fMWrUzVIDPYLCHe2xvp9kZOZufXgyE5wze30A6S1HhYbMbNNCql08lmP0wQ3l%2Fp8fBF6hmgoQ73vZPphwCEOQxonP7IzQJC9%2Bl03cbmmPDij%2BBHrrczU55456whyF167TTNstajIJ4rERfcYdlkv3VOQEaXo%2BsUUrdXbI6wjb9vErff5hUgaW2%2FMl%2FZjNZthSCybk58RIUT3ndyGTtBSg%2B3hP4C4%2FvzRl3TXL0yiIELKVkzrELbYENRqWib%2B5aGXN1a54ll48VdKQiJFSZhEfYp27AW49Qxe7epmorgOuUBd76FZwMZCR%2Bg1QRnSS21%2F0PRVHSvWj3BDeST7nIue51s83rsZb9rkrE52ADZNwBV5mFrcRYyQoaKe%22%7D"
 
-var url_浦发储蓄卡金豆页面 = "spdbbank://wap.spdb.com.cn/awakeapp?login_flag=0&support_type=1&path=vue|mspmk-cli-welfare/goldenBean&APP_VERSION=@appVersion&from=shouye&login_flag=0&support_type=1"
+let url_浦发储蓄卡金豆页面 = "spdbbank://wap.spdb.com.cn/awakeapp?login_flag=0&support_type=1&path=vue|mspmk-cli-welfare/goldenBean&APP_VERSION=@appVersion&from=shouye&login_flag=0&support_type=1"
 
-var url_招商便民 = "cmbmobilebank://cmbls/functionjump?action=gocorpno&corpno=791166&cmb_app_trans_parms_start=here&channel=share&appflag=0";
+let url_招商便民 = "cmbmobilebank://cmbls/functionjump?action=gocorpno&corpno=791166&cmb_app_trans_parms_start=here&channel=share&appflag=0";
 
-var url_ccb签到 = "ccbapp://utils?ccbEncodeParam=ruapsj7I2qua8pR6GnzLuNDjkpEFwI6MLyIzj8A6hPaK13CAzdfO7%2F6FFEhz6IaYUiKwTF8ckUCewiBaPOk9a0gAifErjtXiC0HouhULgghYW08MmyPehRxGnG7a2aIGU2QjeJhu7V0b6NBf0Dn4bm%2BVZNZM91%2FAchT1Q3CtLi5xA7XtYGFbHP%2FS3%2FE%3D"
+let url_ccb签到 = "ccbapp://utils?ccbEncodeParam=ruapsj7I2qua8pR6GnzLuNDjkpEFwI6MLyIzj8A6hPaK13CAzdfO7%2F6FFEhz6IaYUiKwTF8ckUCewiBaPOk9a0gAifErjtXiC0HouhULgghYW08MmyPehRxGnG7a2aIGU2QjeJhu7V0b6NBf0Dn4bm%2BVZNZM91%2FAchT1Q3CtLi5xA7XtYGFbHP%2FS3%2FE%3D"
 
-var url_招商饭票签到 = "cmbmobilebank://cmbls/functionjump?action=gocorpno&corpno=100856&cmb_app_trans_parms_start=here&param=v2&appflag=0"
+let url_招商饭票签到 = "cmbmobilebank://cmbls/functionjump?action=gocorpno&corpno=100856&cmb_app_trans_parms_start=here&param=v2&appflag=0"
 
-var url_ysf会员中心 = "upwallet://applet?encryptAppId=472741b326b7bb5c&toLink=https%3A%2F%2Fcloudvip.95516.com%2F&scenarioId=1006"
+let url_ysf会员中心 = "upwallet://applet?encryptAppId=472741b326b7bb5c&toLink=https%3A%2F%2Fcloudvip.95516.com%2F&scenarioId=1006"
 
-var url_中信 = "citicbankdkkj://go?dl=ctrl%3A%2F%2FcommonWap%7B%22wapType%22%3A%22noMerchantNum%22%2C%22merchantNum%22%3A%22%22%2C%22merchantUrl%22%3A%22https%3A%2F%2Fmtp.creditcard.ecitic.com%2Fciticcard%2Fmtplocallife%2Findex.html%23%2FgoodsDetail%3Fitem_id%3D000001774871d8380001ab01%22%2C%22utm_info%22%3A%7B%22utm_campaign%22%3A%22%22%2C%22utm_entrance%22%3A%22%22%2C%22utm_source%22%3A%22%22%2C%22utm_term%22%3A%22%22%7D%7D"
+let url_中信 = "citicbankdkkj://go?dl=ctrl%3A%2F%2FcommonWap%7B%22wapType%22%3A%22noMerchantNum%22%2C%22merchantNum%22%3A%22%22%2C%22merchantUrl%22%3A%22https%3A%2F%2Fmtp.creditcard.ecitic.com%2Fciticcard%2Fmtplocallife%2Findex.html%23%2FgoodsDetail%3Fitem_id%3D000001774871d8380001ab01%22%2C%22utm_info%22%3A%7B%22utm_campaign%22%3A%22%22%2C%22utm_entrance%22%3A%22%22%2C%22utm_source%22%3A%22%22%2C%22utm_term%22%3A%22%22%7D%7D"
+
+let url_jd_陪伴计划签到 = "openApp.jdMobile://virtual?params=%7B%22category%22%3A%22jump%22%2C%22des%22%3A%22m%22%2C%22sourceValue%22%3A%22babel-act%22%2C%22sourceType%22%3A%22babel%22%2C%22url%22%3A%22https%3A%2F%2Fprodev.m.jd.com%2Fmall%2Factive%2FkPM3Xedz1PBiGQjY4ZYGmeVvrts%2Findex.html%3F_ts%3D1639699578309%26utm_user%3Dplusmember%26gx%3DRnE1kGNcbjCMy9RP--txW_9EV8uJtA6p-gpG%26ad_od%3Dshare%26utm_source%3Dandroidapp%26utm_medium%3Dappshare%26utm_campaign%3Dt_335139774%26utm_term%3DWxfriends%22%2C%22M_sourceFrom%22%3A%22H5%22%2C%22msf_type%22%3A%22click%22%2C%22m_param%22%3A%7B%22m_source%22%3A%220%22%2C%22event_series%22%3A%7B%7D%2C%22jda%22%3A%22122270672.1639699665213436427917.1639699665.1639699665.1639699665.1%22%2C%22usc%22%3A%22androidapp%22%2C%22ucp%22%3A%22t_335139774%22%2C%22umd%22%3A%22appshare%22%2C%22utr%22%3A%22Wxfriends%22%2C%22jdv%22%3A%22122270672%7Candroidapp%7Ct_335139774%7Cappshare%7CWxfriends%7C1639700793905%22%2C%22ref%22%3A%22https%3A%2F%2Fprodev.m.jd.com%2Fmall%2Factive%2FkPM3Xedz1PBiGQjY4ZYGmeVvrts%2Findex.html%3F_ts%3D1639699578309%26utm_user%3Dplusmember%26gx%3DRnE1kGNcbjCMy9RP--txW_9EV8uJtA6p-gpG%26ad_od%3Dshare%26utm_source%3Dandroidapp%26utm_medium%3Dappshare%26utm_campaign%3Dt_335139774%26utm_term%3DWxfriends%22%2C%22psn%22%3A%221639699665213436427917%7C1%22%2C%22psq%22%3A4%2C%22unpl%22%3A%22%22%2C%22pc_source%22%3A%22%22%2C%22mba_muid%22%3A%221639699665213436427917%22%2C%22mba_sid%22%3A%2216396996652221252864885512314%22%2C%22std%22%3A%22MO-J2011-1%22%2C%22par%22%3A%22_ts%3D1639699578309%26utm_user%3Dplusmember%26gx%3DRnE1kGNcbjCMy9RP--txW_9EV8uJtA6p-gpG%26ad_od%3Dshare%26utm_source%3Dandroidapp%26utm_medium%3Dappshare%26utm_campaign%3Dt_335139774%26utm_term%3DWxfriends%22%2C%22event_id%22%3A%22Babel_CommonExpo%22%2C%22mt_xid%22%3A%22%22%2C%22mt_subsite%22%3A%22%22%2C%22YINLIUhuanqi%22%3A%22https%3A%2F%2Fprodev.m.jd.com%2Fmall%2Factive%2FkPM3Xedz1PBiGQjY4ZYGmeVvrts%2Findex.html%3F_ts%3D1639699578309%26utm_user%3Dplusmember%26gx%3DRnE1kGNcbjCMy9RP--txW_9EV8uJtA6p-gpG%26ad_od%3Dshare%26utm_source%3Dandroidapp%26utm_medium%3Dappshare%26utm_campaign%3Dt_335139774%26utm_term%3DWxfriends%22%2C%22toappactive%22%3A%221%22%7D%2C%22SE%22%3A%7B%22mt_subsite%22%3A%22%22%2C%22__jdv%22%3A%22122270672%7Candroidapp%7Ct_335139774%7Cappshare%7CWxfriends%7C1639700793905%22%2C%22unpl%22%3A%22%22%2C%22__jda%22%3A%22122270672.1639699665213436427917.1639699665.1639699665.1639699665.1%22%7D%7D;package=com.jingdong.app.mall;end"
 
 app.startActivity({
     action: "android.intent.action.VIEW",
-    data: url_中信,
+    data: url_jd_陪伴计划签到,
 });
 
 // --------------url scheme--------------
--- 每日签到.js
+++ 每日签到.js
@@ -1,23 +1,25 @@
 auto.waitFor();
 var func = require("func_list.js");
 var devModel = device.model;;
+var devMate30, devHonor8, devRedMi;
+devMate30 = "TAS-AL00";
+devHonor8 = "FRD-AL00";
+devRedMi = "Redmi Note 7";
 
 main();
 
 function main() {
-    var devMate30, devHonor8, devRedMi;
-    devMate30 = "TAS-AL00";
-    devHonor8 = "FRD-AL00";
-    devRedMi = "Redmi Note 7";
     // 中行缤纷生活() 邮储银行() 邮储信用卡() 华彩生活() 招商银行()
     zs = 招商();
     ysf = 云闪付();
+    jd = 京东();
     if (devModel == devMate30) {
         // 龙支付签到();
         浦发银行();
         中国农业银行();
         什么值得买();
-        jd_sign();
+        jd.jd_sign();
+        jd.陪伴签到();
         ysf.签到();
         ysf.领积点();
         浦发信用卡();
@@ -28,7 +30,8 @@ function main() {
     } else if (devModel == devHonor8) {
         ysf.签到();
         ysf.领积点();
-        jd_sign();
+        jd.jd_sign();
+        jd.陪伴签到();
         zs.便民生活();
         zs.饭票签到();
     } else if (devModel == devRedMi) {
@@ -46,7 +49,7 @@ function main() {
 function 云闪付() {
     this.领积点 = function () {
         func.toAutojs();
-        var url_ysf会员中心 = "upwallet://applet?encryptAppId=472741b326b7bb5c&toLink=https%3A%2F%2Fcloudvip.95516.com%2F&scenarioId=1006"
+        let url_ysf会员中心 = "upwallet://applet?encryptAppId=472741b326b7bb5c&toLink=https%3A%2F%2Fcloudvip.95516.com%2F&scenarioId=1006"
         app.startActivity({
             action: "android.intent.action.VIEW",
             data: url_ysf会员中心,
@@ -63,7 +66,7 @@ function 云闪付() {
     }
 
     this.签到 = function () {
-        var appName = "com.unionpay";
+        let appName = "com.unionpay";
         //closeApp(appName);
         func.toPackage(appName);
         while (className("TextView").text("我的").findOnce() == null) {
@@ -94,12 +97,12 @@ function 云闪付() {
 
 
 function 工商() {
-    var appName = "中国工商银行";
+    let appName = "中国工商银行";
     //closeApp(appName);
     func.toAutojs();
 
-    var url_工行小象 = "com.icbc.androidclient://startType=PORTALINJECT&menuId=xiaoxiangleyuan&shareCurrentUUID=";
-    // var url_工行小象 = "com.icbc.androidclient://startType=PORTALINJECT&menuId=taskCenter&injectParams=dGFyZ2V0PWVsZmxk&shareCurrentUUID="
+    let url_工行小象 = "com.icbc.androidclient://startType=PORTALINJECT&menuId=xiaoxiangleyuan&shareCurrentUUID=";
+    // let url_工行小象 = "com.icbc.androidclient://startType=PORTALINJECT&menuId=taskCenter&injectParams=dGFyZ2V0PWVsZmxk&shareCurrentUUID="
     // 启动小象
     app.startActivity({
         action: "android.intent.action.VIEW",
@@ -117,14 +120,14 @@ function 工商() {
     sleep(2000);
     // 点击香蕉
     // 点击任务
-    var left_banana, mission_btn;
+    let left_banana, mission_btn;
     left_banana = textStartsWith("剩余").findOne();
     sleep(1000);
     // 查找并点击香蕉
-    var bananas;
+    let bananas;
     bananas = className("ListView").rowCount(5).findOnce();
     if (bananas != null) {
-        for (var i = bananas.childCount() - 1; i >= 0; i--) {
+        for (let i = bananas.childCount() - 1; i >= 0; i--) {
             // 点击喂小象
             func.cClick(left_banana);
             sleep(1000);
@@ -152,12 +155,12 @@ function 工商() {
 function 招商() {
     this.饭票签到 = function () {
         func.toAutojs();
-        var url_招商饭票签到 = "cmbmobilebank://cmbls/functionjump?action=gocorpno&corpno=100856&cmb_app_trans_parms_start=here&param=v2&appflag=0"
+        let url_招商饭票签到 = "cmbmobilebank://cmbls/functionjump?action=gocorpno&corpno=100856&cmb_app_trans_parms_start=here&param=v2&appflag=0"
         app.startActivity({
             action: "android.intent.action.VIEW",
             data: url_招商饭票签到,
         });
-        var congratulation, getAir, item_parent, today = null;
+        let congratulation, getAir, item_parent, today = null;
         while (today == null) {
             congratulation = textContains("恭喜您在").findOnce();
             if (congratulation != null) {
@@ -188,8 +191,8 @@ function 招商() {
     }
     this.便民生活 = function () {
         func.toAutojs();
-        var appName = "招商银行";
-        var url_招商便民 = "cmbmobilebank://cmbls/functionjump?action=gocorpno&corpno=791166&cmb_app_trans_parms_start=here&channel=share&appflag=0";
+        let appName = "招商银行";
+        let url_招商便民 = "cmbmobilebank://cmbls/functionjump?action=gocorpno&corpno=791166&cmb_app_trans_parms_start=here&channel=share&appflag=0";
         app.startActivity({
             action: "android.intent.action.VIEW",
             data: url_招商便民,
@@ -199,7 +202,7 @@ function 招商() {
         sleep(500);
         func.gesture_pwd(appName);
         sleep(2000);
-        var my_energy, get_energy;
+        let my_energy, get_energy;
         while (text("查询我的公积金").findOnce() == null) {
             my_energy = text("我的能量：").findOnce();
             if (my_energy != null) {
@@ -210,8 +213,8 @@ function 招商() {
             sleep(2000);
         }
         sleep(2000);
-        var plus30, plus_parent, plus_parent_childcount;
-        var sign_btn, sign_text;
+        let plus30, plus_parent, plus_parent_childcount;
+        let sign_btn, sign_text;
 
         plus30 = text("+30").depth(14).findOne();
         plus_parent = plus30.parent().parent();
@@ -254,7 +257,7 @@ function 招商() {
 // 中行缤纷生活
 function 中行缤纷生活() {
     // test3
-    var appName = "缤纷生活";
+    let appName = "缤纷生活";
     //closeApp(appName);
     func.toApp(appName);
     while (text("我的").findOnce() == null) {
@@ -266,7 +269,7 @@ function 中行缤纷生活() {
         sleep(1000);
     }
     // 签到按钮
-    var signBtnId = "imgRight";
+    let signBtnId = "imgRight";
     while (id(signBtnId).findOnce() == null) {
         func.toAutojs();
         func.toApp(appName);
@@ -286,14 +289,14 @@ function 中行缤纷生活() {
     sleep(2500);
     text("查看活力奖励>").findOne();
 
-    var currentWeekday = new Date().getDay();
+    let currentWeekday = new Date().getDay();
     // 0 返回的周日 周一返回1，周二2
     if (currentWeekday == 0) {
         currentWeekday = 6
     } else {
         currentWeekday = currentWeekday - 1
     }
-    var signFlag, idx, weekdayText, signCnt;
+    let signFlag, idx, weekdayText, signCnt;
     signCnt = 0;
 
     while (true) {
@@ -329,10 +332,10 @@ function 中行缤纷生活() {
 
 // 农行小豆
 function 中国农业银行() {
-    var appName = "中国农业银行";
+    let appName = "中国农业银行";
     //closeApp(appName);
     func.toAutojs();
-    var url_农行小豆 = "bankabc://%7B%22method%22%3A%22jumpToSharedProduct%22%2C%22param%22%3A%22rR4uOmAzpF49gqwDYQiLp20AltfnLciJg3Fyp5ijEIlD6KSfPdLMNyKsM8JboO6MwU4dRe9KEsPXqC4shEX19X6hEiWyiILqbgLFXv9xJ5Jc7WP8cgtQBKyWQwTlznpR47%2BlnPSHUcgGQwprcCrZljQQsb3H9RhiJ2D2qeBt4JJz84Yh2iQ9R1lu%2FY%2BWKtaP25m0LbNLiCBYzuVXpAI%2BZfQKjVDNu72M0bgLPJtM1yg7oAXVGsadNuQMbKRz0XWTmkZzKVNYYupr4XqG08l6VvoOh1qETuzMO5mSCup%2FrhpJbwn4v5yYWC68q2FmK6K8YXpHZRtZyVIQwKrZKYjGCZ%2BdeHNIQKJe2plRNjawvy1QfB%2FYEIxcT68HT63j3KJK0%2FxlZSrAvT9cbSKRHkxleVMdKn8uj8HVMWs8l1DMdrLJK0tNFerEfKnSptOj4bSiQ6kvE4M2fMWrUzVIDPYLCHe2xvp9kZOZufXgyE5wze30A6S1HhYbMbNNCql08lmP0wQ3l%2Fp8fBF6hmgoQ73vZPphwCEOQxonP7IzQJC9%2Bl03cbmmPDij%2BBHrrczU55456whyF167TTNstajIJ4rERfcYdlkv3VOQEaXo%2BsUUrdXbI6wjb9vErff5hUgaW2%2FMl%2FZjNZthSCybk58RIUT3ndyGTtBSg%2B3hP4C4%2FvzRl3TXL0yiIELKVkzrELbYENRqWib%2B5aGXN1a54ll48VdKQiJFSZhEfYp27AW49Qxe7epmorgOuUBd76FZwMZCR%2Bg1QRnSS21%2F0PRVHSvWj3BDeST7nIue51s83rsZb9rkrE52ADZNwBV5mFrcRYyQoaKe%22%7D"
+    let url_农行小豆 = "bankabc://%7B%22method%22%3A%22jumpToSharedProduct%22%2C%22param%22%3A%22rR4uOmAzpF49gqwDYQiLp20AltfnLciJg3Fyp5ijEIlD6KSfPdLMNyKsM8JboO6MwU4dRe9KEsPXqC4shEX19X6hEiWyiILqbgLFXv9xJ5Jc7WP8cgtQBKyWQwTlznpR47%2BlnPSHUcgGQwprcCrZljQQsb3H9RhiJ2D2qeBt4JJz84Yh2iQ9R1lu%2FY%2BWKtaP25m0LbNLiCBYzuVXpAI%2BZfQKjVDNu72M0bgLPJtM1yg7oAXVGsadNuQMbKRz0XWTmkZzKVNYYupr4XqG08l6VvoOh1qETuzMO5mSCup%2FrhpJbwn4v5yYWC68q2FmK6K8YXpHZRtZyVIQwKrZKYjGCZ%2BdeHNIQKJe2plRNjawvy1QfB%2FYEIxcT68HT63j3KJK0%2FxlZSrAvT9cbSKRHkxleVMdKn8uj8HVMWs8l1DMdrLJK0tNFerEfKnSptOj4bSiQ6kvE4M2fMWrUzVIDPYLCHe2xvp9kZOZufXgyE5wze30A6S1HhYbMbNNCql08lmP0wQ3l%2Fp8fBF6hmgoQ73vZPphwCEOQxonP7IzQJC9%2Bl03cbmmPDij%2BBHrrczU55456whyF167TTNstajIJ4rERfcYdlkv3VOQEaXo%2BsUUrdXbI6wjb9vErff5hUgaW2%2FMl%2FZjNZthSCybk58RIUT3ndyGTtBSg%2B3hP4C4%2FvzRl3TXL0yiIELKVkzrELbYENRqWib%2B5aGXN1a54ll48VdKQiJFSZhEfYp27AW49Qxe7epmorgOuUBd76FZwMZCR%2Bg1QRnSS21%2F0PRVHSvWj3BDeST7nIue51s83rsZb9rkrE52ADZNwBV5mFrcRYyQoaKe%22%7D"
 
     app.startActivity({
         action: "android.intent.action.VIEW",
@@ -359,7 +362,7 @@ function 中国农业银行() {
 
 // 邮储银行
 function 邮储银行() {
-    var appName = "邮储银行";
+    let appName = "邮储银行";
     //closeApp(appName);
     func.toApp(appName);
     while (className("RadioButton").text("我的").findOnce() == null) {
@@ -390,10 +393,10 @@ function 邮储银行() {
 
 // 浦发金豆签到
 function 浦发银行() {
-    var appName = "浦发银行";
+    let appName = "浦发银行";
     //closeApp(appName);
     func.toAutojs();
-    var url_浦发储蓄卡金豆页面 = "spdbbank://wap.spdb.com.cn/awakeapp?login_flag=0&support_type=1&path=vue|mspmk-cli-welfare/goldenBean&APP_VERSION=@appVersion&from=shouye&login_flag=0&support_type=1";
+    let url_浦发储蓄卡金豆页面 = "spdbbank://wap.spdb.com.cn/awakeapp?login_flag=0&support_type=1&path=vue|mspmk-cli-welfare/goldenBean&APP_VERSION=@appVersion&from=shouye&login_flag=0&support_type=1";
     app.startActivity({
         action: "android.intent.action.VIEW",
         data: url_浦发储蓄卡金豆页面,
@@ -414,9 +417,9 @@ function 浦发银行() {
     sleep(2000);
     textContains("+").findOne();
     sleep(800);
-    var signs = textContains("+").find();
+    let signs = textContains("+").find();
     try {
-        for (var i = 0; i < signs.length; i++) {
+        for (let i = 0; i < signs.length; i++) {
             if (signs[i].text().length <= 10) {
                 func.sClick(signs[i]);
             }
@@ -430,78 +433,103 @@ function 浦发银行() {
     sleep(3000);
 }
 
-function jd_sign() {
-    func.toApp("京东");
-    //等待首页加载
-    while (text("首页").findOnce() == null) {
-        func.sClick(id("xk").findOnce());
-        toastLog("等待首页...");
-        func.passAd();
-        func.sClick(textContains("取消").findOnce());
-        func.sClick(descContains("取消").findOnce());
-        sleep(1500);
-    }
-    var getBeans = text("领京豆").findOne();
-    func.sClick(getBeans.parent());
-    // 等待页面加载
-    // textContains("购物返豆").findOne();
-    // sleep(800);
-
-    while (textContains("已连").findOnce() == null) {
-        if (func.cClick(text("签到领京豆").findOnce())) {
-            toastLog("已点击 签到领京豆");
-            sleep(2000);
+function 京东() {
+    this.jd_sign = function () {
+        func.toApp("京东");
+        //等待首页加载
+        while (text("首页").findOnce() == null) {
+            func.sClick(id("xk").findOnce());
+            toastLog("等待首页...");
+            func.passAd();
+            func.sClick(textContains("取消").findOnce());
+            func.sClick(descContains("取消").findOnce());
+            sleep(1500);
         }
-        if (text("签到日历").findOnce() != null) {
-            sleep(2000);
+        let getBeans = text("领京豆").findOne();
+        func.sClick(getBeans.parent());
+        // 等待页面加载
+        // textContains("购物返豆").findOne();
+        // sleep(800);
+
+        while (textContains("已连").findOnce() == null) {
+            if (func.cClick(text("签到领京豆").findOnce())) {
+                toastLog("已点击 签到领京豆");
+                sleep(2000);
+            }
+            if (text("签到日历").findOnce() != null) {
+                sleep(2000);
+                back();
+                sleep(2000);
+            }
+            sleep(800);
+        }
+        toastLog("已签到");
+        sleep(1000);
+
+        while (text("首页").findOnce() == null) {
             back();
             sleep(2000);
         }
-        sleep(800);
-    }
-    toastLog("已签到");
-    sleep(1000);
+        let getCon = text("领券").findOne();
+        func.sClick(getCon.parent());
 
-    while (text("首页").findOnce() == null) {
-        back();
-        sleep(2000);
-    }
-    var getCon = text("领券").findOne();
-    func.sClick(getCon.parent());
-
-    while (className("ImageView").desc("领券中心").findOnce() == null) {
-        // var closeBtn = id("com.jd.lib.coupon.feature:id/db").findOnce();
-        // if (closeBtn != null) {
-        //     func.sClick(closeBtn.parent().child(1));
+        while (className("ImageView").desc("领券中心").findOnce() == null) {
+            // let closeBtn = id("com.jd.lib.coupon.feature:id/db").findOnce();
+            // if (closeBtn != null) {
+            //     func.sClick(closeBtn.parent().child(1));
+            // }
+            sleep(800);
+        }
+        sleep(1200);
+        let signBtn;
+        signBtn = text("签到领奖励").findOnce();
         // }
-        sleep(800);
-    }
-    sleep(1200);
-    var signBtn;
-    signBtn = text("签到领奖励").findOnce();
-    // }
 
-    if (signBtn == null) {
-        toastLog("今日已领券");
-    }
-    else {
-        func.sClick(signBtn);
-        sleep(2000);
-        className("ImageView").desc("关闭弹窗").findOne();
-        func.sClick(className("ImageView").desc("关闭弹窗").findOne());
-        toastLog("今日已领券");
-        sleep(1000);
-    }
+        if (signBtn == null) {
+            toastLog("今日已领券");
+        }
+        else {
+            func.sClick(signBtn);
+            sleep(2000);
+            className("ImageView").desc("关闭弹窗").findOne();
+            func.sClick(className("ImageView").desc("关闭弹窗").findOne());
+            toastLog("今日已领券");
+            sleep(1000);
+        }
 
-    while (text("首页").findOnce() == null) {
-        back();
-        sleep(2000);
+        while (text("首页").findOnce() == null) {
+            back();
+            sleep(2000);
+        }
     }
+    this.陪伴签到 = function () {
+        let signed, unsign, txt1, txt2;
+        txt1 = "陪伴频道签到赚京豆";
+        txt2 = "活动规则";
+        signed = "今日已签";
+        unsign = "签到";
+        let url_jd_陪伴计划签到 = "openApp.jdMobile://virtual?params=%7B%22category%22%3A%22jump%22%2C%22des%22%3A%22m%22%2C%22sourceValue%22%3A%22babel-act%22%2C%22sourceType%22%3A%22babel%22%2C%22url%22%3A%22https%3A%2F%2Fprodev.m.jd.com%2Fmall%2Factive%2FkPM3Xedz1PBiGQjY4ZYGmeVvrts%2Findex.html%3F_ts%3D1639699578309%26utm_user%3Dplusmember%26gx%3DRnE1kGNcbjCMy9RP--txW_9EV8uJtA6p-gpG%26ad_od%3Dshare%26utm_source%3Dandroidapp%26utm_medium%3Dappshare%26utm_campaign%3Dt_335139774%26utm_term%3DWxfriends%22%2C%22M_sourceFrom%22%3A%22H5%22%2C%22msf_type%22%3A%22click%22%2C%22m_param%22%3A%7B%22m_source%22%3A%220%22%2C%22event_series%22%3A%7B%7D%2C%22jda%22%3A%22122270672.1639699665213436427917.1639699665.1639699665.1639699665.1%22%2C%22usc%22%3A%22androidapp%22%2C%22ucp%22%3A%22t_335139774%22%2C%22umd%22%3A%22appshare%22%2C%22utr%22%3A%22Wxfriends%22%2C%22jdv%22%3A%22122270672%7Candroidapp%7Ct_335139774%7Cappshare%7CWxfriends%7C1639700793905%22%2C%22ref%22%3A%22https%3A%2F%2Fprodev.m.jd.com%2Fmall%2Factive%2FkPM3Xedz1PBiGQjY4ZYGmeVvrts%2Findex.html%3F_ts%3D1639699578309%26utm_user%3Dplusmember%26gx%3DRnE1kGNcbjCMy9RP--txW_9EV8uJtA6p-gpG%26ad_od%3Dshare%26utm_source%3Dandroidapp%26utm_medium%3Dappshare%26utm_campaign%3Dt_335139774%26utm_term%3DWxfriends%22%2C%22psn%22%3A%221639699665213436427917%7C1%22%2C%22psq%22%3A4%2C%22unpl%22%3A%22%22%2C%22pc_source%22%3A%22%22%2C%22mba_muid%22%3A%221639699665213436427917%22%2C%22mba_sid%22%3A%2216396996652221252864885512314%22%2C%22std%22%3A%22MO-J2011-1%22%2C%22par%22%3A%22_ts%3D1639699578309%26utm_user%3Dplusmember%26gx%3DRnE1kGNcbjCMy9RP--txW_9EV8uJtA6p-gpG%26ad_od%3Dshare%26utm_source%3Dandroidapp%26utm_medium%3Dappshare%26utm_campaign%3Dt_335139774%26utm_term%3DWxfriends%22%2C%22event_id%22%3A%22Babel_CommonExpo%22%2C%22mt_xid%22%3A%22%22%2C%22mt_subsite%22%3A%22%22%2C%22YINLIUhuanqi%22%3A%22https%3A%2F%2Fprodev.m.jd.com%2Fmall%2Factive%2FkPM3Xedz1PBiGQjY4ZYGmeVvrts%2Findex.html%3F_ts%3D1639699578309%26utm_user%3Dplusmember%26gx%3DRnE1kGNcbjCMy9RP--txW_9EV8uJtA6p-gpG%26ad_od%3Dshare%26utm_source%3Dandroidapp%26utm_medium%3Dappshare%26utm_campaign%3Dt_335139774%26utm_term%3DWxfriends%22%2C%22toappactive%22%3A%221%22%7D%2C%22SE%22%3A%7B%22mt_subsite%22%3A%22%22%2C%22__jdv%22%3A%22122270672%7Candroidapp%7Ct_335139774%7Cappshare%7CWxfriends%7C1639700793905%22%2C%22unpl%22%3A%22%22%2C%22__jda%22%3A%22122270672.1639699665213436427917.1639699665.1639699665.1639699665.1%22%7D%7D;package=com.jingdong.app.mall;end"
+
+        app.startActivity({
+            action: "android.intent.action.VIEW",
+            data: url_jd_陪伴计划签到,
+        });
+        while (!(text(txt1).findOnce() != null && text(txt2).findOnce() != null)) {
+            sleep(800);
+        }
+        sleep(3500);
+        if (text(signed).findOnce() == null) {
+            func.sClick(text(unsign).findOnce());
+        }
+        toastLog("已签到");
+        sleep(3000);
+    }
+    return this;
 }
 
 // 买单吧
 function 买单吧() {
-    var appName = "买单吧";
+    let appName = "买单吧";
     //closeApp(appName);
     func.toApp(appName);
     while (className("TextView").id("tv_title").text("我的").findOnce() == null) {
@@ -531,7 +559,7 @@ function 买单吧() {
 
 // 浦发
 function 浦发信用卡() {
-    var appName = "浦大喜奔";
+    let appName = "浦大喜奔";
     //closeApp(appName);
     func.toApp(appName);
     while (text("我的").findOnce() == null) {
@@ -557,7 +585,7 @@ function 浦发信用卡() {
     text("天天领福利").findOne();
     sleep(1000);
     if (text("已签到").findOnce() == null) {
-        var waitSign = text("立即签到").findOne();
+        let waitSign = text("立即签到").findOne();
         sleep(1000);
         // func.sClick(waitSign.parent().parent().parent().parent().child(4));
         func.sClick(waitSign);
@@ -568,12 +596,12 @@ function 浦发信用卡() {
 
 // 邮储信用卡
 function 邮储信用卡() {
-    var appName = "邮储信用卡";
+    let appName = "邮储信用卡";
     func.toApp(appName);
     while (text("我的").findOnce() == null) {
         func.passAd();
         func.sClick(text("确认").findOnce());
-        var Continue = className("TextView").text("继续使用").findOnce();
+        let Continue = className("TextView").text("继续使用").findOnce();
         if (Continue != null) {
             func.sClick(Continue);
         }
@@ -600,7 +628,7 @@ function 邮储信用卡() {
 
 // 华彩生活
 function 华彩生活() {
-    var appName = "华彩生活";
+    let appName = "华彩生活";
     //closeApp(appName);
     func.toApp(appName);
     while (text("我的").findOnce() == null) {
@@ -633,7 +661,7 @@ function 华彩生活() {
 }
 
 function 工银e生活() {
-    var appName = "工银e生活";
+    let appName = "工银e生活";
     //closeApp(appName);
     func.toApp(appName);
     while (id("radio_button1").text("生活").findOnce() == null) {
@@ -661,9 +689,9 @@ function 工银e生活() {
 }
 
 function 什么值得买() {
-    var appName = "什么值得买";
+    let appName = "什么值得买";
     func.toApp(appName);
-    var signBtn = null;
+    let signBtn = null;
     while (signBtn == null) {
         signBtn = id("tv_login_sign").findOnce();
         func.sClick(id("tab_usercenter").text("我的").findOnce());
